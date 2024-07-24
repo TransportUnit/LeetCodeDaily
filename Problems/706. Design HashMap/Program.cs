2537e674ca2d2db923a7aa2377e3b69f46dfa06a
Thomas Werner <thomas.werner@utikal-automation.com>
2024-07-24 21:23:47 +0200

    added 706

---
+++ Problems/706. Design HashMap/Program.cs
@@ -0,0 +1,35 @@
+﻿using _706._Design_HashMap;
+using LeetCodeDaily.Core;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase<Func<MyHashMap, int>, int>(
+        hashMap =>
+        {
+            hashMap.Put(1, 1);
+            hashMap.Put(2, 2);
+            return hashMap.Get(1);
+        },
+        1)
+    .CreateCase(
+        hashMap =>
+        {
+            return hashMap.Get(3);
+        },
+        -1)
+    .CreateCase(
+        hashMap =>
+        {
+            hashMap.Put(2, 1);
+            return hashMap.Get(2);
+        },
+        1)
+    .CreateCase(
+        hashMap =>
+        {
+            hashMap.Remove(2);
+            return hashMap.Get(2);
+        },
+        -1)
+    .Run();
