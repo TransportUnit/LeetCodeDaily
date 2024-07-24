74a0ceea21548fd61447d5f624dfbe17901d3717
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-07-24 21:36:03 +0200

    added 217

---
+++ Problems/217. Contains Duplicate/Program.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        "[1,2,3,1]".ParseArray<int>(),
+        true)
+    .CreateCase(
+        "[1,2,3,4]".ParseArray<int>(),
+        false)
+    .CreateCase(
+        "[1,1,1,3,3,4,3,2,4,2]".ParseArray<int>(),
+        true)
+    .Run();
