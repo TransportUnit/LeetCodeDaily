e6456495391b9824ecc268c5d9654959dda1f92d
Thomas Werner <thomas.werner@utikal-automation.com>
2024-07-24 20:54:28 +0200

    added 2181

---
+++ Problems/2181. Merge Nodes in Between Zeros/Program.cs
@@ -0,0 +1,17 @@
+﻿using LeetCodeDaily.Core;
+
+ResultGeneratorAttribute.Detect();
+
+Case.SetResultChecker<ListNode, ListNode>(c =>
+{
+    return c.ActualResult!.ToString().Equals(c.ExpectedResult.ToString());
+});
+
+Case
+    .CreateCase(
+        ListNode.Parse("[0,3,1,0,4,5,2,0]"),
+        ListNode.Parse("[4,11]"))
+    .CreateCase(
+        ListNode.Parse("[0,1,0,3,0,2,2,0]"),
+        ListNode.Parse("[1,3,4]"))
+    .Run();
---
+++ Problems/2181. Merge Nodes in Between Zeros/Solution.cs
@@ -0,0 +1,39 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2181._Merge_Nodes_in_Between_Zeros;
+
+public class Solution
+{
+    [ResultGenerator]
+    public ListNode MergeNodes(ListNode head)
+    {
+        var current = head;
+        var iterator = head.next;
+
+        while (iterator != null)
+        {
+            current.val += iterator.val;
+            if (iterator.val == 0 && iterator.next != null)
+            {
+                current = current.next;
+                current!.val = 0;
+            }
+            iterator = iterator.next;
+        }
+
+        current.next = null;
+        return head;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
