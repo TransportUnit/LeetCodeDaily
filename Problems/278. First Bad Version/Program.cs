086b75b482136dc1611a85431c5476ab0eb574b4
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-07-24 21:31:00 +0200

    added 278

---
+++ Problems/278. First Bad Version/Program.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeDaily.Core;
+
+ResultGeneratorAttribute.Detect();
+
+var solution = ResultGeneratorAttribute.SolutionInstance as _278._First_Bad_Version.Solution;
+
+solution!.SetBadVersion(4);
+Case
+    .CreateCase(
+        5,
+        4)
+    .Run();
+
+
+solution.SetBadVersion(1);
+Case
+    .CreateCase(
+        1,
+        1)
+    .Run();
+
+
+solution.SetBadVersion(1702766719);
+Case
+    .CreateCase(
+        2126753390,
+        1702766719)
+    .Run();
---
+++ Problems/278. First Bad Version/VersionControl.cs
@@ -0,0 +1,16 @@
+﻿namespace _278._First_Bad_Version;
+
+public class VersionControl
+{
+    private int _badVersion;
+
+    public bool IsBadVersion(int version)
+    {
+        return version >= _badVersion;
+    }
+
+    public void SetBadVersion(int version)
+    {
+        _badVersion = version;
+    }
+}
