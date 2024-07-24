74286531ca13992600cf46d6b953bdc5406cea7a
Thomas Werner <thomas.werner@utikal-automation.com>
2024-07-24 21:36:03 +0200

    added 217

---
+++ Problems/217. Contains Duplicate/README.md
@@ -0,0 +1,27 @@
+# 217. Contains Duplicate
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+<br/>
+
+Example 1:
+- Input: nums = [1,2,3,1]
+- Output: true
+
+<br/>
+
+Example 2:
+- Input: nums = [1,2,3,4]
+- Output: false
+
+<br/>
+
+Example 3:
+- Input: nums = [1,1,1,3,3,4,3,2,4,2]
+- Output: true
+
+<br/>
+
+Constraints:
+- 1 &lt;= nums.length &lt;= 105
+- -109 &lt;= nums[i] &lt;= 109
---
+++ Problems/217. Contains Duplicate/Solution.cs
@@ -0,0 +1,40 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _217._Contains_Duplicate;
+
+public class Solution
+{
+    // O(n) time, O(n) space
+    [ResultGenerator]
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var hash = new HashSet<int>();
+        hash.Add(nums[0]);
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (!hash.Add(nums[i]))
+                return true;
+        }
+
+        return false;
+    }
+
+    /*
+	// O(n*log(n) + n) time, O(1) space
+    public bool ContainsDuplicate(int[] nums) 
+    {
+        Array.Sort(nums);
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] == nums[i - 1])
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    */
+}
