9f0f8a2edd72af20db131013b1b0bec8f639c724
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-07-24 20:33:56 +0200

    added 2094

---
+++ Problems/2094. Finding 3-Digit Even Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        "[2,1,3,0]".ParseArray<int>(),
+        "[102,120,130,132,210,230,302,310,312,320]".ParseArray<int>())
+    .CreateCase(
+        "[2,2,8,8,2]".ParseArray<int>(),
+        "[222,228,282,288,822,828,882]".ParseArray<int>())
+    .CreateCase(
+        "[3,7,5]".ParseArray<int>(),
+        Array.Empty<int>())
+    .Run();
---
+++ Problems/2094. Finding 3-Digit Even Numbers/Solution.cs
@@ -0,0 +1,53 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2094._Finding_3_Digit_Even_Numbers;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int[] FindEvenNumbers(int[] digits)
+    {
+        int[] freq = new int[10];
+        foreach (var digit in digits)
+        {
+            freq[digit]++;
+        }
+
+        var result = new List<int>();
+        int i, j, k;
+
+        for (i = 1; i < 10; i++)
+        {
+            if (freq[i] == 0)
+            {
+                continue;
+            }
+
+            freq[i]--;
+
+            for (j = 0; j < 10; j++)
+            {
+                if (freq[j] == 0)
+                {
+                    continue;
+                }
+
+                freq[j]--;
+
+                for (k = 0; k < 10; k += 2)
+                {
+                    if (freq[k] == 0)
+                        continue;
+
+                    result.Add(i * 100 + j * 10 + k);
+                }
+
+                freq[j]++;
+            }
+
+            freq[i]++;
+        }
+
+        return result.ToArray();
+    }
+}
