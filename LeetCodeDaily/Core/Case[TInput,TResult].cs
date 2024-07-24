1aff19936ab29e96897352e0a4df22cc0110446f
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-07-24 19:37:04 +0200

    Add project files.

---
+++ LeetCodeDaily/Core/Case[TInput,TResult].cs
@@ -0,0 +1,139 @@
+﻿using LeetCodeDaily.Extensions;
+using System.Collections;
+using System.Diagnostics;
+
+namespace LeetCodeDaily.Core;
+
+public class Case<TInput, TResult>
+{
+    public static Func<TInput, TResult>? ResultGenerator { get; set; }
+    public static Func<Case<TInput, TResult>, bool>? ResultChecker { get; set; }
+
+    public string InputString { get; set; } = string.Empty;
+    public TInput Input { get; set; }
+    public TResult ExpectedResult { get; set; }
+    public TResult? ActualResult { get; set; }
+    public bool Passed { get; set; }
+    public IList<Case<TInput, TResult>>? SubCases { get; set; }
+    public TimeSpan ExecutionTime { get; set; }
+
+    static Case()
+    {
+        Case.SetResultChecker<TInput, TResult>(c =>
+        {
+            // this is sufficient for value types (such as int, string, etc.)
+            if (
+                c.ExpectedResult is IEnumerable expectedEnumerable &&
+                c.ActualResult is IEnumerable resultEnumerable)
+            {
+                var expectedEnumerator = expectedEnumerable.GetEnumerator();
+                var resultEnumerator = resultEnumerable.GetEnumerator();
+
+                try
+                {
+                    // basically a copy of the System.Linq.Enumerable.SequenceEqual extension method that uses
+                    // the generic IEnumerable type instead of IEnumerable<TSource>
+                    while (expectedEnumerator.MoveNext())
+                    {
+                        if (!resultEnumerator.MoveNext() || !expectedEnumerator.Current.Equals(resultEnumerator.Current))
+                        {
+                            return false;
+                        }
+                    }
+
+                    if (resultEnumerator.MoveNext())
+                    {
+                        return false;
+                    }
+
+                    return true;
+                }
+                finally
+                {
+                    if (expectedEnumerator is IDisposable expectedDisposable)
+                    {
+                        expectedDisposable.Dispose();
+                    }
+                    if (resultEnumerator is IDisposable resultDisposable)
+                    {
+                        resultDisposable.Dispose();
+                    }
+                }
+            }
+
+            return c.ActualResult switch
+            {
+                null => c.ExpectedResult is null,
+                not null => c.ActualResult.Equals(c.ExpectedResult)
+            };
+        });
+    }
+
+    public Case(TInput input, TResult expectedResult)
+    {
+        Input = input;
+        ExpectedResult = expectedResult;
+    }
+
+    public Case<TInput, TResult> GenerateResult()
+    {
+        if (ResultGenerator is null)
+        {
+            throw new InvalidOperationException("ResultGenerator delegate was not set.");
+        }
+
+        var stopwatch = Stopwatch.StartNew();
+        ActualResult = ResultGenerator(Input);
+        stopwatch.Stop();
+        ExecutionTime = stopwatch.Elapsed;
+        return this;
+    }
+
+    public bool ResultAsExpected()
+    {
+        if (ResultChecker is null)
+        {
+            throw new InvalidOperationException("ResultChecker delegate was not set.");
+        }
+
+        return ResultChecker(this);
+    }
+
+    public Case<TInput, TResult> Run(bool printResult = true)
+    {
+        GenerateResult();
+        Passed = ResultAsExpected();
+        if (printResult)
+            PrintResult();
+
+        if (SubCases is not null)
+        {
+            foreach (var subCase in SubCases)
+            {
+                subCase.Run(printResult);
+            }
+        }
+        return this;
+    }
+
+    public Case<TInput, TResult> PrintResult()
+    {
+        $"Test {(Passed ? "passed" : "failed")}.".Print();
+        $"Input: {Input.TryGetObjectString()}".Print();
+        $"Result: {ActualResult.TryGetObjectString()}".Print();
+        $"Expected: {ExpectedResult.TryGetObjectString()}".Print();
+        $"Time: {ExecutionTime.TotalMilliseconds.TryGetObjectString()} ms".Print();
+        string.Empty.Print();
+        return this;
+    }
+
+    public Case<TInput, TResult> CreateCase(TInput input, TResult expectedResult)
+    {
+        if (SubCases is null)
+            SubCases = new List<Case<TInput, TResult>>();
+
+        var subCase = new Case<TInput, TResult>(input, expectedResult);
+        SubCases.Add(subCase);
+        return this;
+    }
+}
---
+++ LeetCodeDaily/Core/ResultGeneratorAttribute.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace LeetCodeDaily.Core;
+
+public class ResultGeneratorAttribute : Attribute
+{
+    public static object? SolutionInstance { get; private set; } = null;
+
+    public static void Detect()
+    {
+        var entryAssembly = Assembly.GetEntryAssembly();
+
+        if (entryAssembly is null)
+        {
+            throw new NullReferenceException("Entry assembly was null");
+        }
+
+        var solutionType =
+            entryAssembly
+                .GetTypes()
+                .FirstOrDefault(
+                    x => x.GetMethods().Any(y => y.GetCustomAttributes(typeof(ResultGeneratorAttribute)).Any()));
+
+        if (solutionType == null)
+        {
+            throw new InvalidOperationException("Could not find solution class type.");
+        }
+
+        SolutionInstance = Activator.CreateInstance(solutionType);
+
+        if (SolutionInstance is null)
+        {
+            throw new InvalidOperationException("Could not create an instance of the solution class");
+        }
+
+        var resultGeneratorMethodInfo =
+            solutionType
+                .GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                .FirstOrDefault(x => x.GetCustomAttributes(typeof(ResultGeneratorAttribute)).Any());
+
+        if (resultGeneratorMethodInfo == null)
+        {
+            throw new InvalidOperationException("Could not find result generator method.");
+        }
+
+        var @delegate = CreateDelegate(resultGeneratorMethodInfo, SolutionInstance);
+        Case.SetResultGenerator(@delegate);
+    }
+
+    private static Delegate CreateDelegate(MethodInfo methodInfo, object target)
+    {
+        var parmTypes = methodInfo.GetParameters().Select(parm => parm.ParameterType);
+        var parmAndReturnTypes = parmTypes.Append(methodInfo.ReturnType).ToArray();
+        var delegateType = Expression.GetDelegateType(parmAndReturnTypes);
+
+        if (methodInfo.IsStatic)
+            return methodInfo.CreateDelegate(delegateType);
+        return methodInfo.CreateDelegate(delegateType, target);
+    }
+}
