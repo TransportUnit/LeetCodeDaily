be0c4bfb789094a97345ba963ec11631f84a2e4a
Thomas Werner <thomas.werner@utikal-automation.com>
2024-07-24 20:41:03 +0200

    added 2108

---
+++ Problems/2108. Find First Palindromic String in the Array/Program.cs
@@ -0,0 +1,22 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        "[abc,car,ada,racecar,cool]".ParseArray<string>(),
+        "ada")
+    .CreateCase(
+        "[notapalindrome,racecar]".ParseArray<string>(),
+        "racecar")
+    .CreateCase(
+        "[def,ghi]".ParseArray<string>(),
+        string.Empty)
+    .CreateCase(
+        "[po,zsz]".ParseArray<string>(),
+        "zsz")
+    .CreateCase(
+        "[cqllrtyhw,swwisru,gpzmbders,wqibjuqvs,pp,usewxryy,ybqfuh,hqwwqftgyu,jggmatpk]".ParseArray<string>(),
+        "pp")
+    .Run();
