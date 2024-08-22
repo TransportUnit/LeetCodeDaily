5e4171ea4c10eddebd58801a714b7bc8403783c9
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-08-22 12:41:57 +0200

    added 476

---
+++ Problems/476. Number Complement/Program.cs
@@ -0,0 +1,15 @@
+﻿using LeetCodeDaily.Core;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        5,
+        2)
+    .CreateCase(
+        1,
+        0)
+    .CreateCase(
+        19,
+        12)
+    .Run();
---
+++ Problems/476. Number Complement/Solution.cs
@@ -0,0 +1,12 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _476._Number_Complement;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int FindComplement(int num)
+    {
+        return ~num & ((1 << (int)(Math.Log2(num) + 1)) - 1);
+    }
+}
