7d30ab04afddb5a7553ca4ef72fa32c670616c94
Thomas Werner <thomas.werner@utikal-automation.com>
2024-08-29 11:31:04 +0200

    added 20

---
+++ Problems/20. Valid Parentheses/Program.cs
@@ -0,0 +1,22 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        "()",
+        true)
+    .CreateCase(
+        "()[]{}",
+        true)
+    .CreateCase(
+        "(]",
+        false)
+      .CreateCase(
+        "([])",
+        true)
+      .CreateCase(
+        "((",
+        false)
+    .Run();
---
+++ Problems/20. Valid Parentheses/Solution.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeDaily.Core;
+
+public class Solution
+{
+    [ResultGenerator]
+    public bool IsValid(string s)
+    {
+        if (s.Length < 2)
+            return false;
+
+        var stack = new Stack<char>();
+
+        foreach (var ch in s)
+        {
+            if (ch == '(' || ch == '[' || ch == '{')
+            {
+                stack.Push(ch);
+            }
+            else if (stack.Count > 0)
+            {
+                var top = stack.Pop();
+                if (ch != top + 1 &&
+                    ch != top + 2)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
