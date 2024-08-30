f0ed86c195023d7c0c73495dd077a3c1676c19f8
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-08-30 15:22:39 +0200

    added 35

---
+++ Problems/35. Search Insert Position/Program.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        ("[1,3,5,6]".ParseArray<int>(), 5),
+        2)
+    .CreateCase(
+        ("[1,3,5,6]".ParseArray<int>(), 2),
+        1)
+    .CreateCase(
+        ("[1,3,5,6]".ParseArray<int>(), 7),
+        4)
+     .CreateCase(
+        ("[1]".ParseArray<int>(), 0),
+        0)
+     .CreateCase(
+        ("[1]".ParseArray<int>(), 2),
+        1)
+     .CreateCase(
+        ("[1,3,5,6,8]".ParseArray<int>(), 7),
+        4)
+     .CreateCase(
+        ("[1,3]".ParseArray<int>(), 2),
+        1)
+    .Run();
