5ae47c64e74bbe8f76d0cdb1ec298d55c296bf16
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-09-02 13:42:55 +0200

    added 1894

---
+++ Problems/1894. Find the Student that Will Replace the Chalk/Program.cs
@@ -0,0 +1,13 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        ("[5,1,5]".ParseArray<int>(), 22),
+        0)
+    .CreateCase(
+        ("[3,4,1,2]".ParseArray<int>(), 25),
+        1)
+    .Run();
---
+++ Problems/1894. Find the Student that Will Replace the Chalk/Solution.cs
@@ -0,0 +1,35 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1894._Find_the_Student_that_Will_Replace_the_Chalk;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int ChalkReplacer(int[] chalk, int k)
+    {
+        long sumChalkConsumption = 0;
+        long k_ = k;
+
+        for (int i = 0; i < chalk.Length; i++)
+        {
+            sumChalkConsumption += chalk[i];
+
+            if (sumChalkConsumption > k_)
+            {
+                return i;
+            }
+        }
+
+        k_ = k_ % sumChalkConsumption;
+
+        for (int i = 0; i < chalk.Length; i++)
+        {
+            if (k_ < chalk[i])
+                return i;
+
+            k_ -= chalk[i];
+        }
+
+        return -1;
+    }
+}
