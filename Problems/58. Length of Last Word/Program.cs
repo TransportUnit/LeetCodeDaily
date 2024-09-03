87ffdf90b47cf76be8991e0d8aa6eeb1bb63c208
Thomas Werner <thomas.werner@utikal-automation.com>
2024-09-03 16:07:29 +0200

    added 58

---
+++ Problems/58. Length of Last Word/Program.cs
@@ -0,0 +1,15 @@
+﻿using LeetCodeDaily.Core;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        "Hello World",
+        5)
+    .CreateCase(
+        "   fly me   to   the moon  ",
+        4)
+    .CreateCase(
+        "luffy is still joyboy",
+        6)
+    .Run();
