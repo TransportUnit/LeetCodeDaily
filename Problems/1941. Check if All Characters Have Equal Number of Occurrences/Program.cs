d8864261deeffd3bc46b6dbdbd104840c8058cae
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-09-03 15:45:23 +0200

    added 1941

---
+++ Problems/1941. Check if All Characters Have Equal Number of Occurrences/Program.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        "abacbc",
+        true)
+    .CreateCase(
+        "aaabb",
+        false)
+    .CreateCase(
+        "abcdefghijklmnopqrstuvwxyzz",
+        false)
+    .Run();
---
+++ Problems/1941. Check if All Characters Have Equal Number of Occurrences/Solution.cs
@@ -0,0 +1,39 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1941._Check_if_All_Characters_Have_Equal_Number_of_Occurrences;
+
+public class Solution
+{
+    [ResultGenerator]
+    public bool AreOccurrencesEqual(string s)
+    {
+        var freq = new short[26];
+
+        foreach (var c in s)
+        {
+            freq[c - 97]++;
+        }
+
+        //return freq.Distinct().Where(x => x > 0).Count() == 1;
+
+
+        int count = -1;
+
+        for (int i = 0; i < freq.Length; i++)
+        {
+            if (freq[i] == 0)
+                continue;
+
+            if (count == -1)
+            {
+                count = freq[i];
+                continue;
+            }
+
+            if (freq[i] != count)
+                return false;
+        }
+
+        return true;
+    }
+}
