10de8982919156cdbdf704da0ad3fcb0ec3991d3
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-09-03 14:47:03 +0200

    added 1945

---
+++ Problems/1945. Sum of Digits of String After Convert/Solution.cs
@@ -0,0 +1,29 @@
+﻿using LeetCodeDaily.Core;
+
+public class Solution
+{
+    [ResultGenerator]
+    private int GetLucky(string s, int k)
+    {
+        // offset: -96 (ASCII 'a': dec '97')
+        var digitSum = s.Sum(c => ((c - 96) / 10) + ((c - 96) % 10));
+
+        while (--k > 0 && digitSum > 9)
+        {
+            digitSum = DigitSum(digitSum);
+        }
+
+        return digitSum;
+    }
+
+    private int DigitSum(int i)
+    {
+        var sum = 0;
+        while (i > 0)
+        {
+            sum += i % 10;
+            i /= 10;
+        }
+        return sum;
+    }
+}
