1d14afc925d3d811fed553cc5a3e4531d725e338
Thomas Werner <thomas.werner@utikal-automation.com>
2024-09-03 14:47:03 +0200

    added 1945

---
+++ Problems/1945. Sum of Digits of String After Convert/Program.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        ("iiii", 1),
+        36)
+    .CreateCase(
+        ("leetcode", 2),
+        6)
+     .CreateCase(
+        ("zbax", 2),
+        8)
+    .Run();
