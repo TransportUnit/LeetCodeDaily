6beffedc5b344a2918b075ba12eb84d7eecd8587
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-09-03 16:07:29 +0200

    added 58

---
+++ Problems/58. Length of Last Word/Solution.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeDaily.Core;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int LengthOfLastWord(string s)
+    {
+        int length = 0;
+
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] == ' ')
+            {
+                if (length == 0)
+                    continue;
+                else
+                    return length;
+            }
+
+            length++;
+        }
+
+        return length;
+    }
+}
