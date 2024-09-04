f2f274302728bc5d68c24cb88cc2a13ed06d7f89
Thomas Werner <thomas.werner@utikal-automation.com>
2024-09-04 18:07:03 +0200

    added 136

---
+++ Problems/136. Single Number/Solution.cs
@@ -0,0 +1,17 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _136._Single_Number;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int SingleNumber(int[] nums)
+    {
+        var result = 0;
+        foreach (var num in nums)
+        {
+            result ^= num;
+        }
+        return result;
+    }
+}
