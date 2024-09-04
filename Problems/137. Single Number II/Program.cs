6994782de4383eebbd7b8df1e2187d2314ffa0b0
Thomas Werner <thomas.werner@utikal-automation.com>
2024-09-04 22:21:24 +0200

    added approach to 137

--- Problems/137. Single Number II/Program.cs
+++ Problems/137. Single Number II/Program.cs
@@ -1,16 +1,23 @@
 ﻿using LeetCodeDaily.Core;
 using LeetCodeDaily.Extensions;
 
+var baseCase = 
+    Case
+        .CreateCase(
+            "[2,2,3,2]".ParseArray<int>(),
+            3)
+        .CreateCase(
+            "[0,1,0,1,0,1,99]".ParseArray<int>(),
+            99)
+        .CreateCase(
+            "[67, 1, 99, 1, 1, 67, 67]".ParseArray<int>(),
+            99)
+        ;
+
 ResultGeneratorAttribute.Detect();
 
-Case
-    .CreateCase(
-        "[2,2,3,2]".ParseArray<int>(),
-        3)
-    .CreateCase(
-        "[0,1,0,1,0,1,99]".ParseArray<int>(),
-        99)
-    .CreateCase(
-        "[67, 1, 99, 1, 1, 67, 67]".ParseArray<int>(),
-        99)
-    .Run();
+baseCase.Run();
+
+ResultGeneratorAttribute.Detect(1);
+
+baseCase.Run();
--- Problems/137. Single Number II/Solution.cs
+++ Problems/137. Single Number II/Solution.cs
@@ -7,6 +7,9 @@ public class Solution
     [ResultGenerator]
     public int SingleNumber(int[] nums)
     {
+        // this SO answer explains this approach pretty well:
+        // https://stackoverflow.com/a/59867358/20120008
+
         var one = 0;
         var two = 0;
 
@@ -16,5 +19,34 @@ public class Solution
             two = (two ^ num) & ~one;
         }
         return one;
+    }
+
+    [ResultGenerator(ApproachIndex = 1)]
+    public int SingleNumberNaive(int[] nums)
+    {
+        int ans = 0;
+        // Iterate through each bit position (0 to 31)
+        for (int i = 31; i >= 0; i--)
+        {
+            int sum = 0;
+            // Count how many numbers have the ith bit set
+            foreach (int num in nums)
+            {
+                int n = (num >> i);
+                if ((n & 1) == 1)
+                {
+                    sum++;
+                }
+            }
+            // If sum is not a multiple of 3, the unique number has this bit set
+            sum = sum % 3;
+            // Shift ans left before adding the bit
+            ans = (ans << 1);
+            if (sum != 0)
+            {
+                ans += 1;
+            }
+        }
+        return ans;
     }
 }
