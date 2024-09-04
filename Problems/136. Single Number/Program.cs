ee85b70a5ad0cc468fcdd7b3e3f7331ea112dfe7
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-09-04 18:07:03 +0200

    added 136

---
+++ Problems/136. Single Number/Program.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        "[2,2,1]".ParseArray<int>(),
+        1)
+    .CreateCase(
+        "[4,1,2,1,2]".ParseArray<int>(),
+        4)
+    .CreateCase(
+        "[1]".ParseArray<int>(),
+        1)
+    .Run();
