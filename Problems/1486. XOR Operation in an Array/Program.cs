903c4924bc64db8edfe9234705ec3402b1fcfa2d
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-09-06 23:33:22 +0200

    added 1486

---
+++ Problems/1486. XOR Operation in an Array/Program.cs
@@ -0,0 +1,32 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+var baseCase =
+    Case
+        .CreateCase(
+            (5, 0),
+            8)
+        .CreateCase(
+            (4, 3),
+            8)
+        .CreateCase(
+            (13, 11),
+            11)
+        .CreateCase(
+            (49, 40),
+            136)
+        .CreateCase(
+            (51, 4),
+            106)
+        .CreateCase(
+            (3, 17),
+            23)
+        .CreateCase(
+            (1, 0),
+            0);
+
+ResultGeneratorAttribute.Detect();
+baseCase.Run();
+
+ResultGeneratorAttribute.Detect(1);
+baseCase.Run();
