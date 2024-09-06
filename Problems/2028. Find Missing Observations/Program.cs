a6badf460382eb7890a999208b5bdb161e97d2b9
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-09-06 21:33:41 +0200

    added 2028, 3217

---
+++ Problems/2028. Find Missing Observations/Program.cs
@@ -0,0 +1,34 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        ("[1,5,6]".ParseArray<int>(), 3, 11),
+        "[3,3,3,3,3,3,3,3,3,2,1]".ParseArray<int>()
+        )
+    .CreateCase(
+        ("[3,2,4,3]".ParseArray<int>(), 4, 2),
+        "[6,6]".ParseArray<int>()
+        )
+    .CreateCase(
+        ("[1,5,6]".ParseArray<int>(), 3, 4),
+        "[2,3,2,2]".ParseArray<int>()
+        )
+    .CreateCase(
+        ("[1,2,3,4]".ParseArray<int>(), 6, 4),
+        "".ParseArray<int>()
+        )
+    .SetResultChecker(c =>
+    {
+        if ((c.ExpectedResult?.Any() ?? false) &&
+            (c.ActualResult?.Any() ?? false))
+        {
+            return c.ExpectedResult.Sum().Equals(c.ActualResult.Sum()) && c.ActualResult.All(x => 1 <= x && x <= 6);
+        }
+
+        return (c.ExpectedResult == null && c.ActualResult == null) ||
+               (c.ExpectedResult!.Length == 0 && c.ActualResult!.Length == 0);
+    })
+    .Run();
---
+++ Problems/3217. Delete Nodes From Linked List Present in Array/Solution.cs
@@ -0,0 +1,49 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _3217._Delete_Nodes_From_Linked_List_Present_in_Array;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    [ResultGenerator]
+    public ListNode ModifiedList(int[] nums, ListNode head)
+    {
+        var hash = new HashSet<int>(nums);
+
+        // searching for valid head
+        while (hash.Contains(head!.val))
+        {
+            head = head.next!;
+        }
+
+        var current = head;
+
+        while (current != null)
+        {
+            var next = current.next;
+            current.next = null;
+
+            // searching for valid element, then setting it as current.next
+            // and then repeating process for the element we just found until current is null
+            while (next != null && hash.Contains(next.val))
+            {
+                next = next.next;
+            }
+
+            current.next = next;
+            current = next;
+        }
+
+        return head;
+    }
+}
