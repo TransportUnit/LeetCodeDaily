c66bff5f24df12a476fe3da79b8a81473024361e
Thomas Werner <thomas.werner@utikal-automation.com>
2024-09-06 21:33:41 +0200

    added 2028, 3217

---
+++ Problems/2028. Find Missing Observations/Solution.cs
@@ -0,0 +1,58 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2028._Find_Missing_Observations;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int[] MissingRolls(int[] rolls, int mean, int n)
+    {
+        // mean_value * total_rolls = total_result to hit with n + m rolls
+        // total_result - observed_total = difference that has to be totaled in n rolls
+        int sumDiff = mean * (rolls.Length + n) - rolls.Sum();
+
+        // the average value that has to be hit with n rolls
+        double nAverage = sumDiff / (double)n;
+
+        // average value with n rolls is not feasible because we are limited to
+        // using values between 1 and 6 -> return empty array
+        if (nAverage < 1 || nAverage > 6)
+            return Array.Empty<int>();
+
+        // this serves as our template placeholder in the result array
+        var roundedNAverage = (int)Math.Round(nAverage);
+
+        var resultArr = Enumerable.Repeat(roundedNAverage, n).ToArray();
+
+        // we have to modify our template result array by this amount in order to hit
+        // the desired total in n rolls
+        sumDiff = sumDiff - roundedNAverage * n;
+
+        int factor;
+        int maxDiff;
+
+        if (sumDiff < 0)
+        {
+            factor = -1;
+            maxDiff = roundedNAverage - 1;
+            sumDiff *= -1;
+        }
+        else
+        {
+            factor = 1;
+            maxDiff = 6 - roundedNAverage;
+        }
+
+        // modifying our result array until the difference to the desired total is 0
+
+        int i = 0;
+        while (sumDiff != 0)
+        {
+            var diff = Math.Min(maxDiff, sumDiff);
+            resultArr[i++] += diff * factor;
+            sumDiff -= diff;
+        }
+
+        return resultArr;
+    }
+}
---
+++ Problems/3217. Delete Nodes From Linked List Present in Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        ("[1,2,3]".ParseArray<int>(), ListNode.Parse("[1,2,3,4,5]")),
+        ListNode.Parse("[4,5]"))
+    .CreateCase(
+        ("[1]".ParseArray<int>(), ListNode.Parse("[1,2,1,2,1,2]")),
+        ListNode.Parse("[2,2,2]"))
+    .CreateCase(
+        ("[5]".ParseArray<int>(), ListNode.Parse("[1,2,3,4]")),
+        ListNode.Parse("[1,2,3,4]"))
+    .CreateCase(
+        ("[1,7,6,2,4]".ParseArray<int>(), ListNode.Parse("[3,7,1,8,1]")),
+        ListNode.Parse("[3,8]"))
+    .SetResultChecker(c =>
+    {
+        return c.ActualResult!.ToString().Equals(c.ExpectedResult!.ToString());
+    })
+    .Run();
