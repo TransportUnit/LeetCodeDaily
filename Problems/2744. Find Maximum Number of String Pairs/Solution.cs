d15401518e605f4ef2e49a26a2b210f367dbdbcd
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-09-08 21:48:14 +0200

    added 2744

---
+++ Problems/2744. Find Maximum Number of String Pairs/Solution.cs
@@ -0,0 +1,32 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2744._Find_Maximum_Number_of_String_Pairs;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int MaximumNumberOfStringPairs(string[] words)
+    {
+        if (words.Length <= 1)
+            return 0;
+
+        int pairs = 0;
+
+        Span<string> arr = words;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[i][0] != arr[j][1] ||
+                    arr[i][1] != arr[j][0])
+                    continue;
+
+                pairs++;
+                break;
+            }
+        }
+
+        return pairs;
+    }
+}
