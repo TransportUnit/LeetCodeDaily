e7bd18e0e2147cfe7756300847677d66cc6d9e65
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-09-08 22:19:25 +0200

    added 2778

---
+++ Problems/2778. Sum of Squares of Special Elements/Program.cs
@@ -0,0 +1,13 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        ("[1,2,3,4]".ParseArray<int>()),
+        21)
+    .CreateCase(
+        ("[2,7,1,19,18,3]".ParseArray<int>()),
+        63)
+    .Run();
