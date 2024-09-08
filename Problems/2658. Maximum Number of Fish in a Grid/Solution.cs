c6d6db778ff2551a7466ad0a97c20d07ddd10e98
Thomas Werner <twerner@mail.hs-ulm.de>
2024-09-08 21:16:00 +0200

    added 2658

---
+++ Problems/2658. Maximum Number of Fish in a Grid/Solution.cs
@@ -0,0 +1,41 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2658._Maximum_Number_of_Fish_in_a_Grid;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int FindMaxFish(int[][] grid)
+    {
+        int max = 0;
+
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[i].Length; j++)
+            {
+                if (grid[i][j] == 0)
+                    continue;
+
+                max = Math.Max(BFS(grid, i, j), max);
+            }
+        }
+
+        return max;
+    }
+
+    private int BFS(int[][] grid, int i, int j)
+    {
+        if (i < 0 || j < 0 || i >= grid.Length || j >= grid[i].Length || grid[i][j] == 0)
+            return 0;
+
+        var value = grid[i][j];
+        grid[i][j] = 0;
+        
+        value += BFS(grid, i + 1, j);
+        value += BFS(grid, i - 1, j);
+        value += BFS(grid, i, j + 1);
+        value += BFS(grid, i, j - 1);
+
+        return value;
+    }
+}
