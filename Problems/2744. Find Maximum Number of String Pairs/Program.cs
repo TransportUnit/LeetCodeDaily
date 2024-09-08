4dc5a6d489e4bcd2d83955ec25b79d54a4283d3b
Thomas Werner <twerner@mail.hs-ulm.de>
2024-09-08 21:48:14 +0200

    added 2744

---
+++ Problems/2744. Find Maximum Number of String Pairs/Program.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        ("[\"cd\",\"ac\",\"dc\",\"ca\",\"zz\"]".ParseArray<string>()),
+        2)
+    .CreateCase(
+        ("[\"ab\",\"ba\",\"cc\"]".ParseArray<string>()),
+        1)
+    .CreateCase(
+        ("[\"aa\",\"ab\"]".ParseArray<string>()),
+        0)
+    .Run();
