35db955f144f30197e28bec3fb5db64c371106e4
Thomas Werner <twerner@mail.hs-ulm.de>
2024-09-08 22:19:25 +0200

    added 2778

---
+++ Problems/2778. Sum of Squares of Special Elements/Solution.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2778._Sum_of_Squares_of_Special_Elements;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int SumOfSquares(int[] nums)
+    {
+        return
+            nums
+                .Where((x, i) => nums.Length % (i + 1) == 0)
+                .Select(x => x * x)
+                .Sum();
+    }
+}
