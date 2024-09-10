fa157fb3d6724d85ccdf418b2c949b6b258b9f9a
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-09-10 12:56:36 +0200

    added 2807

---
+++ Problems/2807. Insert Greatest Common Divisors in Linked List/Program.cs
@@ -0,0 +1,17 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        ListNode.Parse("[18,6,10,3]"),
+        ListNode.Parse("[18,6,6,2,10,1,3]"))
+    .CreateCase(
+        ListNode.Parse("[7]"),
+        ListNode.Parse("[7]"))
+    .SetResultChecker(c =>
+    {
+        return c.ActualResult!.ToString().Equals(c.ExpectedResult!.ToString());
+    })
+    .Run();
