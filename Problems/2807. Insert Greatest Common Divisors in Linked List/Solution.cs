cec8fc720dcfb5d88cb483ee6619db15f84de7ef
Thomas Werner <thomas.werner@utikal-automation.com>
2024-09-10 12:56:36 +0200

    added 2807

---
+++ Problems/2807. Insert Greatest Common Divisors in Linked List/Solution.cs
@@ -0,0 +1,49 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2807._Insert_Greatest_Common_Divisors_in_Linked_List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    [ResultGenerator]
+    public ListNode InsertGreatestCommonDivisors(ListNode head)
+    {
+        var current = head;
+
+        while (current?.next != null)
+        {
+            current.next =
+                new ListNode(
+                    GCD(current.val, current.next.val),
+                    current.next
+                );
+            current = current.next.next;
+        }
+
+        return head;
+    }
+
+    // i stole this routine
+    private static int GCD(int a, int b)
+    {
+        while (a != 0 && b != 0)
+        {
+            if (a > b)
+                a %= b;
+            else
+                b %= a;
+        }
+
+        return a | b;
+    }
+}
