9c2dcf52b45fd61345e7696a1e32aa20e99a0028
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-09-13 20:38:52 +0200

    added 1309

---
+++ Problems/1309. Decrypt String from Alphabet to Integer Mapping/Program.cs
@@ -0,0 +1,12 @@
+﻿using LeetCodeDaily.Core;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        "10#11#12",
+        "jkab")
+    .CreateCase(
+        "1326#",
+        "acz")
+    .Run();
