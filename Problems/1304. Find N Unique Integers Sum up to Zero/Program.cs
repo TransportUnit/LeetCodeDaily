c81589335d9447d4864a31cca7d9a1475c9c6473
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-09-13 22:07:18 +0200

    added 1304

---
+++ Problems/1304. Find N Unique Integers Sum up to Zero/Program.cs
@@ -0,0 +1,24 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+Case
+    .CreateCase(
+        5,
+        "[-1,1,2,0,-2]".ParseArray<int>())
+    .CreateCase(
+        3,
+        "[1,0,-1]".ParseArray<int>())
+    .CreateCase(
+        1,
+        "[0]".ParseArray<int>())
+    .CreateCase(
+        4,
+        "[2,-1,1,-2]".ParseArray<int>())
+    .CreateCase(
+        6,
+        "[-2,2,1,-1,-3,3]".ParseArray<int>())
+    .SetResultChecker(c => c.ActualResult?.Sum() == c.ExpectedResult?.Sum())
+    .Detect()
+    .Run()
+    .Detect(1)
+    .Run();
---
+++ Problems/1304. Find N Unique Integers Sum up to Zero/Solution.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1304.Find_N_Unique_Integers_Sum_up_to_Zero;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int[] SumZero(int n) 
+    {
+        var result = new int[n];
+
+        for (int i = 0; i < n / 2; i++)
+        {
+            result[i * 2] = i + 1;
+            result[i * 2 + 1] = -(i + 1);
+        }
+
+        if (n % 2 != 0)
+        {
+            result[^1] = 0;
+        }
+
+        return result;
+    }
+
+    [ResultGenerator(ApproachIndex = 1)]
+    public int[] SumZeroLinq(int n)
+    {
+        var arr = Enumerable.Range(-n / 2, n).ToArray();
+        if (n % 2 == 0)
+        {
+            arr[n / 2] = n / 2;
+        }
+        return arr;
+    }
+}
