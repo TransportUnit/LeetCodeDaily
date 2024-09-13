9373a72d6181c04e0dace38876d7b6b3536ff9da
Thomas Werner <thomas.werner@utikal-automation.com>
2024-09-13 16:20:59 +0200

    added 2220

---
+++ Problems/2220. Minimum Bit Flips to Convert Number/Program.cs
@@ -0,0 +1,13 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        (10, 7),
+        3)
+    .CreateCase(
+        (3, 4),
+        3)
+    .Run();
