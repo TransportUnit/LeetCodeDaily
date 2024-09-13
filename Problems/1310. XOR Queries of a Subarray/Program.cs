723e6f9a0af7a3833cb528dddcb3a41fd2015e58
Thomas Werner <thomas.werner@utikal-automation.com>
2024-09-13 18:29:54 +0200

    added 1310

---
+++ Problems/1310. XOR Queries of a Subarray/Program.cs
@@ -0,0 +1,13 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        ("[1,3,4,8]".ParseArray<int>(), "[[0,1],[1,2],[0,3],[3,3]]".ParseMatrix<int>()),
+        "[2,7,14,8]".ParseArray<int>())
+    .CreateCase(
+        ("[4,8,2,10]".ParseArray<int>(), "[[2,3],[1,3],[0,0],[0,3]]".ParseMatrix<int>()),
+        "[8,0,4,4]".ParseArray<int>())
+    .Run();
---
+++ Problems/1310. XOR Queries of a Subarray/Solution.cs
@@ -0,0 +1,45 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1310.XOR_Queries_of_a_Subarray;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int[] XorQueries(int[] arr, int[][] queries)
+    {
+        for (int i = 1; i < arr.Length; i++)
+        {
+            arr[i] ^= arr[i - 1];
+        }
+
+        var result = new int[queries.Length];
+
+        for (int i = 0; i < queries.Length; i++)
+        {
+            result[i] = queries[i][0] == 0 ? arr[queries[i][1]] : arr[queries[i][0] - 1] ^ arr[queries[i][1]];
+        }
+
+        return result;
+    }
+
+    [ResultGenerator(ApproachIndex = 1)]
+    public int[] XorQueriesSlower(int[] arr, int[][] queries)
+    {
+        var prefix = new int[arr.Length];
+        prefix[0] = arr[0];
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            prefix[i] = prefix[i - 1] ^ arr[i];
+        }
+
+        var result = new int[queries.Length];
+
+        for (int i = 0; i < queries.Length; i++)
+        {
+            result[i] = prefix[queries[i][0]] ^ prefix[queries[i][1]] ^ arr[queries[i][0]];
+        }
+
+        return result;
+    }
+}
