d0cef03edd99fa9ab65e9709f7b6a99384862706
Thomas Werner <thomas.werner@utikal-automation.com>
2024-09-13 18:22:07 +0200

    added 1684

---
+++ Problems/1684. Count the Number of Consistent Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+var baseCase =
+    Case
+        .CreateCase(
+            ("ab", "[\"ad\",\"bd\",\"aaab\",\"baa\",\"badab\"]".ParseArray<string>()),
+            2)
+        .CreateCase(
+            ("abc", "[\"a\",\"b\",\"c\",\"ab\",\"ac\",\"bc\",\"abc\"]".ParseArray<string>()),
+            7)
+        .CreateCase(
+            ("cad", "[\"cc\",\"acd\",\"b\",\"ba\",\"bac\",\"bad\",\"ac\",\"d\"]".ParseArray<string>()),
+            4);
+
+ResultGeneratorAttribute.Detect();
+baseCase.Run();
+
+ResultGeneratorAttribute.Detect(1);
+baseCase.Run();
