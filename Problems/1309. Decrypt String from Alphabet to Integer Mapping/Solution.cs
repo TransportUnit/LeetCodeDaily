64d5f15be6bfa68446790212ebec7a5fb197b80f
Thomas Werner <thomas.werner@utikal-automation.com>
2024-09-13 20:38:52 +0200

    added 1309

---
+++ Problems/1309. Decrypt String from Alphabet to Integer Mapping/Solution.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeDaily.Core;
+using System.Text;
+
+namespace _1309.Decrypt_String_from_Alphabet_to_Integer_Mapping
+{
+    public class Solution
+    {
+        [ResultGenerator]
+        public string FreqAlphabets(string s)
+        {
+            var sb = new StringBuilder();
+            var n2 = s.Length - 2;
+            int i = -1;
+
+            while (++i < n2)
+            {
+                if (s[i + 2] != '#')
+                {
+                    sb.Append((char)(s[i] + 48));
+                }
+                else
+                {
+                    sb.Append((char)((s[i] - 48) * 10 + 48 + s[i + 1]));
+                    i += 2;
+                }
+            }
+
+            for (; i < s.Length; i++)
+            {
+                sb.Append((char)(s[i] + 48));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
