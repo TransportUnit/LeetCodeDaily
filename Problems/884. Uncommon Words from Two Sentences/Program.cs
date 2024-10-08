f44cc910aa10a62fb0b5cfdddea12db368d58245
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-10-08 18:36:13 +0200

    added 884

---
+++ Problems/884. Uncommon Words from Two Sentences/Program.cs
@@ -0,0 +1,13 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        ("this apple is sweet", "this apple is sour"),
+        "[\"sweet\",\"sour\"]".ParseArray<string>())
+    .CreateCase(
+        ("apple apple", "banana"),
+        "[\"banana\"]".ParseArray<string>())
+    .Run();
---
+++ Problems/884. Uncommon Words from Two Sentences/Solution.cs
@@ -0,0 +1,14 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _884.Uncommon_Words_from_Two_Sentences;
+
+public class Solution
+{
+    [ResultGenerator]
+    public string[] UncommonFromSentences(string s1, string s2)
+    {
+        var split1 = s1.Split();
+        var split2 = s2.Split();
+        return split1.Concat(split2).GroupBy(x => x).Where(x => x.Count() == 1).Select(x => x.Key).ToArray();
+    }
+}
