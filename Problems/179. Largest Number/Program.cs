58bae496c12e58bbfda81c075dd63fe98c4db9cb
Thomas Werner <thomas.werner@utikal-automation.com>
2024-10-08 18:18:23 +0200

    added 179

---
+++ Problems/179. Largest Number/Program.cs
@@ -0,0 +1,13 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        "[10,2]".ParseArray<int>(),
+        "210")
+    .CreateCase(
+        "[3,30,34,5,9]".ParseArray<int>(),
+        "9534330")
+    .Run();
