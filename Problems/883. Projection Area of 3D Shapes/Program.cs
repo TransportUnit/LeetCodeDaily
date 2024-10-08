fd796a9e0fdc2e87968d92ee654d50773169c378
Thomas Werner <thomas.werner@utikal-automation.com>
2024-10-08 18:40:34 +0200

    added 883

---
+++ Problems/883. Projection Area of 3D Shapes/Program.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        "[[1,2],[3,4]]".ParseMatrix<int>(),
+        17)
+    .CreateCase(
+        "[[2]]".ParseMatrix<int>(),
+        5)
+    .CreateCase(
+        "[[1,0],[0,2]]".ParseMatrix<int>(),
+        8)
+    .Run();
---
+++ Problems/883. Projection Area of 3D Shapes/Solution.cs
@@ -0,0 +1,32 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _883.Projection_Area_of_3D_Shapes;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int ProjectionArea(int[][] grid)
+    {
+        int n = grid.Length;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int rowMax = 0;
+            int colMax = 0;
+
+            for (int j = 0; j < n; j++)
+            {
+                if (grid[i][j] > 0)
+                    sum++;
+
+                rowMax = Math.Max(grid[i][j], rowMax);
+                colMax = Math.Max(grid[j][i], colMax);
+            }
+
+            sum += rowMax + colMax;
+        }
+
+        return sum;
+    }
+}
