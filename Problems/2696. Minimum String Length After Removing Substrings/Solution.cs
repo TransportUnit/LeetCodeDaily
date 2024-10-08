82f36519b8044cb2871b94871de3bdba89316f60
Thomas Werner <thomas.werner@utikal-automation.com>
2024-10-08 18:05:49 +0200

    added 2696

---
+++ Problems/2696. Minimum String Length After Removing Substrings/Solution.cs
@@ -0,0 +1,33 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2696.Minimum_String_Length_After_Removing_Substrings;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int MinLength(string s)
+    {
+        Stack<char> stack = new();
+
+        foreach (var c in s)
+        {
+            if (stack.Count == 0)
+            {
+                stack.Push(c);
+                continue;
+            }
+
+            if (c == 'B' && stack.Peek() == 'A' ||
+                c == 'D' && stack.Peek() == 'C')
+            {
+                stack.Pop();
+            }
+            else
+            {
+                stack.Push(c);
+            }
+        }
+
+        return stack.Count;
+    }
+}
