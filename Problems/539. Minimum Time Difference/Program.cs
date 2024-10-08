e20d33a8f961c77d4a76bd29372963b81843df44
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-10-08 18:31:39 +0200

    added 539

---
+++ Problems/539. Minimum Time Difference/Program.cs
@@ -0,0 +1,15 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        // Apparently, arrays in C# already implement the IList interface,
+        // so we don't need to cast this to a list ('.ToList()') before passing it.
+        "[\"23:59\",\"00:00\"]".ParseArray<string>().ToIList(),
+        1)
+    .CreateCase(
+        "[\"00:00\",\"23:59\",\"00:00\"]".ParseArray<string>().ToIList(),
+        0)
+    .Run();
---
+++ Problems/539. Minimum Time Difference/Solution.cs
@@ -0,0 +1,37 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _539.Minimum_Time_Difference;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int FindMinDifference(IList<string> timePoints)
+    {
+        int minDiff = int.MaxValue;
+
+        var sortedMinutes =
+            timePoints
+                .Select(x => GetMinutes(x))
+                .OrderBy(x => x)
+                .ToList();
+
+        for (int i = 0; i < sortedMinutes.Count - 1; i++)
+        {
+            minDiff = Math.Min(sortedMinutes[i + 1] - sortedMinutes[i], minDiff);
+
+            if (minDiff == 0)
+                return 0;
+        }
+
+        return Math.Min((1440 - sortedMinutes.Last()) + sortedMinutes.First(), minDiff);
+    }
+
+    private int GetMinutes(string timePoint)
+    {
+        return
+            (timePoint[0] - '0') * 600 +
+            (timePoint[1] - '0') * 60 +
+            (timePoint[3] - '0') * 10 +
+            (timePoint[4] - '0');
+    }
+}
