cc0f7fe441a520fd6a82cec4d1cd985f7f62cb01
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-11-28 14:57:53 +0100

    added 2287

---
+++ Problems/2287. Rearrange Characters to Make Target String/Program.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        ("ilovecodingonleetcode", "code"),
+        2)
+    .CreateCase(
+        ("abcba", "abc"),
+        1)
+    .CreateCase(
+        ("abbaccaddaeea", "aaaaa"),
+        1)
+    .Run();
---
+++ Problems/2287. Rearrange Characters to Make Target String/Solution.cs
@@ -0,0 +1,38 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2287.Rearrange_Characters_to_Make_Target_String;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int RearrangeCharacters(string s, string target)
+    {
+        Span<byte> sFreq = stackalloc byte[26];
+        Span<byte> targetFreq = stackalloc byte[26];
+
+        var sSpan = s.AsSpan();
+        var targetSpan = target.AsSpan();
+
+        for (int i = 0; i < sSpan.Length; i++)
+        {
+            sFreq[sSpan[i] - 'a']++;
+        }
+
+        for (int i = 0; i < targetSpan.Length; i++)
+        {
+            targetFreq[targetSpan[i] - 'a']++;
+        }
+
+        var max = int.MaxValue;
+
+        for (int i = 0; i < targetFreq.Length; i++)
+        {
+            if (targetFreq[i] == 0)
+                continue;
+
+            max = Math.Min(max, sFreq[i] / targetFreq[i]);
+        }
+
+        return max;
+    }
+}
