d02ec8f76fce88bd4f973c3ce387a22c0e59fc4f
Thomas Werner <thomas.werner@utikal-automation.com>
2024-11-28 16:17:34 +0100

    added 2283

---
+++ Problems/2283. Check if Number Has Equal Digit Count and Digit Value/Program.cs
@@ -0,0 +1,13 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        "1210",
+        true)
+    .CreateCase(
+        "030",
+        false)
+    .Run();
---
+++ Problems/2283. Check if Number Has Equal Digit Count and Digit Value/Solution.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2283.Check_if_Number_Has_Equal_Digit_Count_and_Digit_Value;
+
+public class Solution
+{
+    [ResultGenerator]
+    public bool DigitCount(string num)
+    {
+        Span<byte> freq = stackalloc byte[10];
+        var numSpan = num.AsSpan();
+
+        for (int i = 0; i < numSpan.Length; i++)
+        {
+            freq[numSpan[i] - '0']++;
+        }
+
+        for (int i = 0; i < numSpan.Length; i++)
+        {
+            if ((numSpan[i] - '0') != freq[i])
+                return false;
+        }
+
+        return true;
+    }
+}
