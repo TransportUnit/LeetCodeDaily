2193dda7ad63bea37e4e424654702195d633eb12
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-11-29 13:15:58 +0100

    added 2574

---
+++ Problems/2574. Left and Right Sum Differences/Program.cs
@@ -0,0 +1,13 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        "[10,4,8,3]".ParseArray<int>(),
+        "[15,1,11,22]".ParseArray<int>())
+    .CreateCase(
+        "[1]".ParseArray<int>(),
+        "[0]".ParseArray<int>())
+    .Run();
