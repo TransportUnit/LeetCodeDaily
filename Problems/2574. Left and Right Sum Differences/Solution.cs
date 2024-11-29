37cab12ef73346e1646494ded338b70ea1f8a259
Thomas Werner <thomas.werner@utikal-automation.com>
2024-11-29 13:15:58 +0100

    added 2574

---
+++ Problems/2574. Left and Right Sum Differences/Solution.cs
@@ -0,0 +1,31 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2574.Left_and_Right_Sum_Differences;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int[] LeftRightDifference(int[] nums)
+    {
+        var result = new int[nums.Length];
+
+        var rightSum = 0;
+        var leftSum = 0;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            rightSum += nums[i];
+        }
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            result[i] = Math.Abs(leftSum - rightSum);
+            leftSum += nums[i];
+            rightSum -= nums[i + 1];
+        }
+
+        result[^1] = Math.Abs(leftSum);
+
+        return result;
+    }
+}
