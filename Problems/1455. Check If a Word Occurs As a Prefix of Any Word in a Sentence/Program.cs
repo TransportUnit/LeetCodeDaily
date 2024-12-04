918ea637b82712f112633474e2d6c4831fbff420
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-12-04 10:24:18 +0100

    added 1455

---
+++ Problems/1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence/Program.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        ("i love eating burger", "burg"),
+        4)
+    .CreateCase(
+        ("this problem is an easy problem", "pro"),
+        2)
+    .CreateCase(
+        ("i am tired", "you"),
+        -1)
+    .Run();
---
+++ Problems/1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence/Solution.cs
@@ -0,0 +1,40 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1455.Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int IsPrefixOfWord(string sentence, string searchWord)
+    {
+        var sentenceSpan = sentence.AsSpan();
+        var searchWordSpan = searchWord.AsSpan();
+
+        int word = 1;
+        int startOfWord = 0;
+        int j = 0;
+
+        for (int i = 0; i < sentenceSpan.Length; i++)
+        {
+            j = i - startOfWord;
+
+            if (j + 1 >= searchWordSpan.Length &&
+                sentenceSpan[i] == searchWordSpan[j])
+            {
+                return word;
+            }
+
+            if (sentenceSpan[i] != searchWordSpan[j])
+            {
+                while (i < sentenceSpan.Length && sentenceSpan[i] != ' ')
+                {
+                    i++;
+                }
+                startOfWord = i + 1;
+                word++;
+            }
+        }
+
+        return -1;
+    }
+}
