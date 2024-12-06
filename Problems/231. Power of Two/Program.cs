114f5193556b62db78c51b01e598a5a7d3e14267
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-12-06 21:38:16 +0100

    added 231

---
+++ Problems/231. Power of Two/Program.cs
@@ -0,0 +1,37 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        1,
+        true)
+    .CreateCase(
+        16,
+        true)
+    .CreateCase(
+        3,
+        false)
+    .CreateCase(
+        -5,
+        false)
+    .CreateCase(
+        -8,
+        false)
+    .CreateCase(
+        0,
+        false)
+    .CreateCase(
+        -2,
+        false)
+    .CreateCase(
+        -2147483648,
+        false)
+    .CreateCase(
+        2147483647,
+        false)
+    .CreateCase(
+        1073741824,
+        true)
+    .Run();
---
+++ Problems/231. Power of Two/Solution.cs
@@ -0,0 +1,10 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _231.Power_of_Two;
+
+public class Solution
+{
+    [ResultGenerator]
+    public bool IsPowerOfTwo(int n) => n > 0 && ((n & (n - 1)) == 0);
+    
+}
