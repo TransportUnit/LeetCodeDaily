01855415948d1e36d4f0e7a7112f328c062ada7b
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-12-09 16:34:36 +0100

    added 3152

---
+++ Problems/3152. Special Array II/Program.cs
@@ -0,0 +1,13 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+ResultGeneratorAttribute.Detect();
+
+Case
+    .CreateCase(
+        ("[3,4,1,2,6]".ParseArray<int>(), "[[0,4]]".ParseMatrix<int>()),
+        "[false]".ParseArray<bool>())
+    .CreateCase(
+        ("[4,3,1,6]".ParseArray<int>(), "[[0,2],[2,3]]".ParseMatrix<int>()),
+        "[false,true]".ParseArray<bool>())
+    .Run();
---
+++ Problems/3152. Special Array II/Solution.cs
@@ -0,0 +1,35 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _3152.Special_Array_II;
+
+public class Solution
+{
+    [ResultGenerator]
+    public bool[] IsArraySpecial(int[] nums, int[][] queries)
+    {
+        // idea:
+        // go through nums and mark every special subarray by replacing all the values
+        // within that area with a unique number (1 for the first area, 2 for the 2nd area and so on)
+        // then, quen querying, check if the entries at those indices have the same value 
+        // (belong to the same contiguous special area)
+
+        var answer = new bool[queries.Length];
+        var last = nums[0];
+        var group = 0;
+        nums[0] = group;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            group += 1 - ((last ^ nums[i]) & 1);
+            last = nums[i];
+            nums[i] = group;
+        }
+
+        for (int i = 0; i < queries.Length; i++)
+        {
+            answer[i] = nums[queries[i][0]] == nums[queries[i][1]];
+        }
+
+        return answer;
+    }
+}
