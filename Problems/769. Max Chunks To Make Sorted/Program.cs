2adc1b4ce465b551d17fbc37e1607187194d2dd6
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2024-12-20 19:58:08 +0100

    added 769

---
+++ Problems/769. Max Chunks To Make Sorted/Program.cs
@@ -0,0 +1,33 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+Case
+    .CreateCase(
+        "[0,1,2,3,4]".ParseArray<int>(),
+        5)
+    .CreateCase(
+        "[4,3,2,1,0]".ParseArray<int>(),
+        1)
+    .CreateCase(
+        "[1,0,2,3,4]".ParseArray<int>(),
+        4)
+    .CreateCase(
+        "[0]".ParseArray<int>(),
+        1)
+    .CreateCase(
+        "[1,0]".ParseArray<int>(),
+        1)
+    .CreateCase(
+        "[0,2,1]".ParseArray<int>(),
+        2)
+    .CreateCase(
+        "[1,2,0,3,5,4]".ParseArray<int>(),
+        3)
+    .CreateCase(
+        "[2,3,4,1,5,0,6,7]".ParseArray<int>(),
+        3)
+    .CreateCase(
+        "[1,4,3,6,0,7,8,2,5]".ParseArray<int>(),
+        1)
+    .Detect()
+    .Run();
---
+++ Problems/769. Max Chunks To Make Sorted/Solution.cs
@@ -0,0 +1,55 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _769.Max_Chunks_To_Make_Sorted;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int MaxChunksToSorted(int[] arr)
+    {
+        int n = arr.Length;
+
+        int chunks = 0;
+
+        int max = 0;
+
+        unsafe
+        {
+            fixed (int* ptr = &arr[0])
+            {
+                int i = -1;
+                int* it = ptr;
+
+                while(++i < n)
+                {
+                    max = max - ((max - *it) & (max - *it++) >> 31);
+                    chunks += max == i ? 1 : 0;
+                }
+            }
+        }
+
+        return chunks;
+    }
+
+    [ResultGenerator(ApproachIndex = 1)]
+    public int MaxChunksToSortedNoPointers(int[] arr)
+    {
+        int n = arr.Length;
+
+        int chunks = 0;
+
+        int max = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            max = Math.Max(max, arr[i]);
+
+            if (max == i)
+            {
+                chunks++;
+            }
+        }
+
+        return chunks;
+    }
+}
