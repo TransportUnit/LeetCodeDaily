8b3880008a74b04811853a37228046617e25ea63
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-04-14 02:55:55 +0200

    added 1922

---
+++ Problems/1922. Count Good Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using LeetCodeDaily.Core;
+
+Case
+    .CreateCase(
+        (long)1, // this cast is necessary so the method signature matches
+        5)
+    .CreateCase(
+        4,
+        400)
+    .CreateCase(
+        50,
+        564908303)
+    .Detect()
+    .Run();
---
+++ Problems/1922. Count Good Numbers/Solution.cs
@@ -0,0 +1,35 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1922._Count_Good_Numbers;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int CountGoodNumbers(long n)
+    {
+        long modulus = 1_000_000_007;
+        long x = ModularPow(5, (n + 1) / 2, modulus);
+        long y = ModularPow(4, n / 2, modulus);
+        long result = (x * y) % modulus;
+
+        return (int)result;
+    }
+
+    private static long ModularPow(long baseValue, long exponent, long modulus)
+    {
+        if (modulus == 1)
+            return 0;
+
+        long result = 1;
+        baseValue = baseValue % modulus;
+
+        while (exponent > 0)
+        {
+            if (exponent % 2 == 1)
+                result = (result * baseValue) % modulus;
+            exponent = exponent >> 1;
+            baseValue = (baseValue * baseValue) % modulus;
+        }
+        return result;
+    }
+}
