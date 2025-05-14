d24ebbf20a510adeaffff3a1315a3efbb713b0ff
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-05-14 15:26:38 +0200

    added 1920

---
+++ Problems/1920. Build Array from Permutation/Program.cs
@@ -0,0 +1,12 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+Case
+    .CreateCase(
+        "[0,2,1,5,3,4]".ParseArray<int>(),
+        "[0,1,2,4,5,3]".ParseArray<int>())
+    .CreateCase(
+        "[5,0,1,2,3,4]".ParseArray<int>(),
+        "[4,5,0,1,2,3]".ParseArray<int>())
+    .Detect()
+    .Run();
---
+++ Problems/1920. Build Array from Permutation/Solution.cs
@@ -0,0 +1,50 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1920.Build_Array_from_Permutation;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int[] BuildArray(int[] nums)
+    {
+        var sp = nums.AsSpan();
+
+        unsafe
+        {
+            fixed (int* head = &sp[0])
+            {
+                int* it = head;
+
+                int n = sp.Length;
+
+                while (--n >= 0)
+                {
+                    *(it + n) += (*(it + *(it + n)) % 1024) * 1024;
+                }
+
+                n = sp.Length;
+
+                while (--n >= 0)
+                {
+                    *(it + n) /= 1024;
+                }
+            }
+        }
+
+        return nums;
+
+        /*
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] += ((nums[nums[i]] % 1000) * 1000);
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] /= 1000;
+        }
+
+        return nums;
+        */
+    }
+}
