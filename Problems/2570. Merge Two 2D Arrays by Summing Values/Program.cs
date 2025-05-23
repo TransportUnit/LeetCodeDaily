f7977936a813330f2eff52716f548f10e61e9095
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-05-23 13:28:49 +0200

    added 2570

---
+++ Problems/2570. Merge Two 2D Arrays by Summing Values/Program.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+Case
+    .CreateCase(
+        ("[[1,2],[2,3],[4,5]]".ParseMatrix<int>(), "[[1,4],[3,2],[4,1]]".ParseMatrix<int>()),
+        "[[1,6],[2,3],[3,2],[4,6]]".ParseMatrix<int>())
+    .CreateCase(
+        ("[[2,4],[3,6],[5,5]]".ParseMatrix<int>(), "[[1,3],[4,3]]".ParseMatrix<int>()),
+        "[[1,3],[2,4],[3,6],[4,3],[5,5]]".ParseMatrix<int>())
+    .Detect()
+    .SetResultChecker(Check)
+    .Run();
+
+static bool Check(Case<(int[][], int[][]), int[][]> c)
+{
+    int i = 0;
+    foreach (var a in c.ExpectedResult)
+    {
+        if (!c.ActualResult[i].SequenceEqual(a))
+        {
+            return false;
+        }
+        i++;
+    }
+
+    return true;
+}
---
+++ Problems/2570. Merge Two 2D Arrays by Summing Values/Solution.cs
@@ -0,0 +1,54 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2570._Merge_Two_2D_Arrays_by_Summing_Values;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int[][] MergeArrays(int[][] nums1, int[][] nums2)
+    {
+        int i = 0;
+        int j = 0;
+
+        int[][] result = new int[nums1.Length + nums2.Length][];
+        int index = 0;
+
+        while (true)
+        {
+            if (i < nums1.Length)
+            {
+                if (j < nums2.Length)
+                {
+                    if (nums1[i][0] < nums2[j][0])
+                    {
+                        result[index++] = nums1[i++];
+                    }
+                    else if (nums1[i][0] > nums2[j][0])
+                    {
+                        result[index++] = nums2[j++];
+                    }
+                    else
+                    {
+                        result[index] = nums1[i];
+                        result[index++][1] = nums1[i++][1] + nums2[j++][1];
+                    }
+                }
+                else
+                {
+                    result[index++] = nums1[i++];
+                }
+            }
+            else if (j < nums2.Length)
+            {
+                result[index++] = nums2[j++];
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        Array.Resize(ref result, index);
+        return result;
+    }
+}
