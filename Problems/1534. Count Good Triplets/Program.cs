5a4ee9c3fcc0680e8fc4353f326f4ae854c39298
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-05-23 13:32:40 +0200

    added 1534

---
+++ Problems/1534. Count Good Triplets/Program.cs
@@ -0,0 +1,12 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+Case
+    .CreateCase(
+        ("[3,0,1,1,9,7]".ParseArray<int>(), 7, 2, 3),
+        4)
+    .CreateCase(
+        ("[1,1,2,2,3]".ParseArray<int>(), 0, 0, 1),
+        0)
+    .Detect()
+    .Run();
---
+++ Problems/1534. Count Good Triplets/Solution.cs
@@ -0,0 +1,32 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1534._Count_Good_Triplets;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int CountGoodTriplets(int[] arr, int a, int b, int c)
+    {
+        int count = 0;
+
+        for (int i = 0; i < arr.Length - 2; i++)
+        {
+            for (int j = i + 1; j < arr.Length - 1; j++)
+            {
+                if (Math.Abs(arr[i] - arr[j]) > a)
+                    continue;
+
+                for (int k = j + 1; k < arr.Length; k++)
+                {
+                    if (Math.Abs(arr[j] - arr[k]) > b ||
+                        Math.Abs(arr[i] - arr[k]) > c)
+                        continue;
+
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
