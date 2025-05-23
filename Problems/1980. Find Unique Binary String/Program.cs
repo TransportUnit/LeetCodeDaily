e206f7f93ed085164f9f45c470b87f9f256aaf72
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-05-23 13:13:22 +0200

    added 1980

---
+++ Problems/1980. Find Unique Binary String/Program.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+Case
+    .CreateCase(
+        "[\"01\",\"10\"]".ParseArray<string>(),
+        "00")
+    .CreateCase(
+        "[\"00\",\"01\"]".ParseArray<string>(),
+        "10")
+    .CreateCase(
+        "[\"111\",\"011\",\"001\"]".ParseArray<string>(),
+        "000")
+    .Detect()
+    .SetResultChecker(Check)
+    .Run();
+
+
+static bool Check(Case<string[], string> c)
+{
+    if (string.IsNullOrEmpty(c.ActualResult))
+        throw new InvalidOperationException("Huh?");
+
+    return
+        c.ActualResult.Length == c.Input.Length &&
+        c.Input.All(num => num != c.ActualResult) &&
+        c.ActualResult.All(ch => ch == '0' || ch == '1');
+}
---
+++ Problems/1980. Find Unique Binary String/Solution.cs
@@ -0,0 +1,33 @@
+﻿using LeetCodeDaily.Core;
+using System.Runtime.InteropServices;
+
+namespace _1980._Find_Unique_Binary_String;
+
+public class Solution
+{
+    [ResultGenerator]
+    public string FindDifferentBinaryString(string[] nums)
+    {
+        int n = nums.Length;
+        Span<char> result = stackalloc char[n + 1];
+
+        var numsSpan = nums.AsSpan();
+
+        unsafe
+        {
+            fixed (char* first = &result[0])
+            {
+                char* iterator = first;
+
+                *(iterator + n) = '\0';
+
+                while (n-- > 0)
+                {
+                    *(iterator + n) = (char)(97 - numsSpan[n][n]);
+                }
+
+                return Marshal.PtrToStringUni((IntPtr)first);
+            }
+        }
+    }
+}
