914f5782b3cc1d582af06d50839a706ba8b4069a
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-05-23 14:55:54 +0200

    added 3024

---
+++ Problems/3024. Type of Triangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+Case
+    .CreateCase(
+        "[9,4,9]".ParseArray<int>(),
+        "isosceles")
+    .CreateCase(
+        "[3,4,5]".ParseArray<int>(),
+        "scalene")
+    .CreateCase(
+        "[1,1,1]".ParseArray<int>(),
+        "equilateral")
+    .CreateCase(
+        "[3,1,2]".ParseArray<int>(),
+        "none")
+    .CreateCase(
+        "[2,5,2]".ParseArray<int>(),
+        "none")
+    .CreateCase(
+        "[3,3,1]".ParseArray<int>(),
+        "isosceles")
+    .CreateCase(
+        "[1,1,2]".ParseArray<int>(),
+        "none")
+    .CreateCase(
+        "[2,3,4]".ParseArray<int>(),
+        "scalene")
+    .Detect()
+    .Run();
---
+++ Problems/3024. Type of Triangle/Solution.cs
@@ -0,0 +1,35 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _3024._Type_of_Triangle;
+
+public class Solution
+{
+    [ResultGenerator]
+    public string TriangleType(int[] nums)
+    {
+        var a = nums[0];
+        var b = nums[1];
+        var c = nums[2];
+
+        var halfCirc = (a + b + c) / 2.0;
+
+        // valid triangle check - no side can be larger than the sum of the other two
+        if (a >= halfCirc || b >= halfCirc || c >= halfCirc)
+        {
+            return "none";
+        }
+
+        // each of those three will be either 0 (if those two sides are equal)
+        // or != 0 (if the sides aren't equal)
+        int t = a ^ b;
+        int z = b ^ c;
+        int u = a ^ c;
+
+        // this expression counts the amount of none zero values from the previous step
+        var x = ((t | -t) >>> 31) + ((z | -z) >>> 31) + ((u | -u) >>> 31);
+
+        return x == 3 ? "scalene" :
+               x == 2 ? "isosceles" :
+               x == 0 ? "equilateral" : "none";
+    }
+}
