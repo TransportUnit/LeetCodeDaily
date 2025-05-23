702c30927150e6c598303428d15bfbed9b584bc4
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-05-23 17:42:09 +0200

    added 3355

---
+++ Problems/3355. Zero Array Transformation I/Program.cs
@@ -0,0 +1,21 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+Case
+    .CreateCase(
+        ("[1,0,1]".ParseArray<int>(), "[[0,2]]".ParseMatrix<int>()),
+        true)
+    .CreateCase(
+        ("[4,3,2,1]".ParseArray<int>(), "[[1,3],[0,2]]".ParseMatrix<int>()),
+        false)
+    .CreateCase(
+        ("[4,3,2,1]".ParseArray<int>(), "[[1,3],[0,2],[0,2],[0,2],[0,2]]".ParseMatrix<int>()),
+        true)
+    .CreateCase(
+        ("[1,2,1,0,0,0]".ParseArray<int>(), "[[0,3],[2,4]]".ParseMatrix<int>()),
+        false)
+    .CreateCase(
+        ("[6,9]".ParseArray<int>(), "[[1,1],[1,1],[0,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[0,1]]".ParseMatrix<int>()),
+        false)
+    .Detect()
+    .Run();
---
+++ Problems/3355. Zero Array Transformation I/Solution.cs
@@ -0,0 +1,37 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _3355._Zero_Array_Transformation_I;
+
+public class Solution
+{
+    [ResultGenerator]
+    public bool IsZeroArray(int[] nums, int[][] queries)
+    {
+        Span<int> diff = stackalloc int[nums.Length + 1];
+
+        foreach (var query in queries)
+        {
+            diff[query[0]] += 1;
+            diff[query[1] + 1] -= 1;
+        }
+
+        if (diff[0] < nums[0])
+        {
+            return false;
+        }
+
+        int current = 0;
+
+        for (int i = 0; i < diff.Length - 1; i++)
+        {
+            current += diff[i];
+
+            if (current < nums[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
