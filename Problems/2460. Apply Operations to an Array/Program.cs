4755de2cb8c73e15bbfc0b38cb80e8814b86a788
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-05-23 13:18:31 +0200

    added 2460

---
+++ Problems/2460. Apply Operations to an Array/Program.cs
@@ -0,0 +1,12 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+Case
+    .CreateCase(
+        "[1,2,2,1,1,0]".ParseArray<int>(),
+        "[1,4,2,0,0,0]".ParseArray<int>())
+    .CreateCase(
+        "[0,1]".ParseArray<int>(),
+        "[1,0]".ParseArray<int>())
+    .Detect()
+    .Run();
---
+++ Problems/2460. Apply Operations to an Array/Solution.cs
@@ -0,0 +1,54 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2460._Apply_Operations_to_an_Array;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int[] ApplyOperations(int[] nums)
+    {
+        int n = nums.Length - 1;
+        int zeroCount = 0;
+        int j = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] == 0)
+            {
+                zeroCount++;
+                continue;
+            }
+
+            if (nums[i] == nums[i + 1])
+            {
+                nums[j++] = nums[i++] * 2;
+                zeroCount++;
+            }
+            else
+            {
+                nums[j++] = nums[i];
+            }
+        }
+
+        if (nums[n] != 0)
+        {
+            nums[j] = nums[n];
+        }
+        else
+        {
+            zeroCount++;
+        }
+
+        if (zeroCount == nums.Length)
+        {
+            return nums;
+        }
+
+        while (zeroCount-- > 0)
+        {
+            nums[n--] = 0;
+        }
+
+        return nums;
+    }
+}
