a9acfa0f1120bd834aee9a181d6d6e580f692d14
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-05-27 00:53:38 +0200

    added 3

---
+++ Problems/3. Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,35 @@
+﻿using LeetCodeDaily.Core;
+
+Case
+    .CreateCase(
+        "abcabcbb",
+        3)
+    .CreateCase(
+        "bbbbb",
+        1)
+    .CreateCase(
+        "pwwkew",
+        3)
+    .CreateCase(
+        " ",
+        1)
+    .CreateCase(
+        "dvdf",
+        3)
+    .CreateCase(
+        "ckilbkd",
+        5)
+    .CreateCase(
+        "tmmzuxt",
+        5)
+    .CreateCase(
+        "abababababababababababababababababababababababababababababababab",
+        2)
+    .Detect(0)
+    .Run()
+    .Detect(1)
+    .Run()
+    .Detect(2)
+    .Run()
+    .Detect(3)
+    .Run();
---
+++ Problems/3. Longest Substring Without Repeating Characters/Solution.cs
@@ -0,0 +1,116 @@
+﻿using LeetCodeDaily.Core;
+using System.Runtime.CompilerServices;
+
+namespace _3._Longest_Substring_Without_Repeating_Characters;
+
+public class Solution
+{
+    [ResultGenerator(ApproachIndex = 0)]
+    public int LengthOfLongestSubstringHashSet(string s)
+    {
+        int n = s.Length;
+
+        if (n < 2)
+            return n;
+
+        HashSet<char> hashSet = new();
+        int l = 0;
+        int r = 0;
+        int max = 0;
+
+        while (r < n)
+        {
+            if (hashSet.Contains(s[r]))
+            {
+                hashSet.Remove(s[l++]);
+            }
+            else
+            {
+                hashSet.Add(s[r++]);
+                max = Math.Max(max, r - l);
+            }
+        }
+
+        return max;
+    }
+
+    [ResultGenerator(ApproachIndex = 1)]
+    public int LengthOfLongestSubstringFreq1(string s)
+    {
+        int n = s.Length;
+
+        if (n < 2)
+            return n;
+
+        Span<ushort> freq = stackalloc ushort[128];
+
+        int l = 0;
+        int r = 0;
+        int max = 0;
+
+        while (r < n)
+        {
+            if (freq[s[r]] > 0)
+            {
+                freq[s[l++]]--;
+            }
+            else
+            {
+                freq[s[r++]]++;
+                max = Math.Max(max, r - l);
+            }
+        }
+
+        return max;
+    }
+
+    [ResultGenerator(ApproachIndex = 2)]
+    public unsafe int LengthOfLongestSubstringFreq2(string s)
+    {
+        int n = s.Length;
+
+        if (n < 2)
+            return n;
+
+        ushort* freq = stackalloc ushort[128];
+        Unsafe.InitBlock(freq, 0, 128 * sizeof(ushort));
+
+        int l = 0, r = 0, max = 0;
+
+        fixed (char* str = s)
+        {
+            while (r < n)
+            {
+                int set = -((int)freq[str[r]]) >>> 31;
+                freq[str[l]] -= (ushort)set;
+                l += set;
+                freq[str[r]] += (ushort)(1 - set);
+                r += (ushort)(1 - set);
+                max = Math.Max(max, r - l);
+            }
+        }
+
+        return max;
+    }
+
+    [ResultGenerator(ApproachIndex = 3)]
+    public int LengthOfLongestSubstringLastIndex(string s)
+    {
+        Span<int> chars = stackalloc int[256];
+        int maxLen = 0, current = 0, left = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            current = s[i];
+            if (chars[current] > 0)
+            {
+
+                left = Math.Max(left, chars[current]);
+            }
+
+            maxLen = Math.Max(maxLen, i - left + 1);
+            chars[current] = i + 1;
+        }
+
+        return maxLen;
+    }
+}
