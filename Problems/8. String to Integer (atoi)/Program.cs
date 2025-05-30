ea214bba7f9da3c531be169dfa0c6b9a711f1ab5
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-05-31 01:37:10 +0200

    added 8

---
+++ Problems/8. String to Integer (atoi)/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using LeetCodeDaily.Core;
+
+Case
+    .CreateCase(
+        "-115579378e25",
+        -115579378)
+    .CreateCase(
+        "   -042",
+        -42)
+    .CreateCase(
+        "1337c0d3",
+        1337)
+    .CreateCase(
+        "0-1",
+        0)
+    .CreateCase(
+        "words and 987",
+        0)
+    .CreateCase(
+        "+-42",
+        0)
+    .CreateCase(
+        "-1123u3761867",
+        -1123)
+    .CreateCase(
+        "2147483648",
+        2147483647)
+    .Detect()
+    .Run();
