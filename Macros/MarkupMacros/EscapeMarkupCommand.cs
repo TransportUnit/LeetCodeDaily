7313db889cfb05cf584cdc6b63d0a1709f84b4b5
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-08-19 17:18:51 +0200

    added markup macros: - surround with superscript tag (<sup>{text}</sup>) - surround with code tag (<code>{text}</code>) - escape markup ("<" -> "&lt;", ">" -> "&gt;", "&" -> "&amp;")

---
+++ Macros/MarkupMacros/EscapeMarkupCommand.cs
@@ -0,0 +1,113 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+namespace MarkupMacros
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class EscapeMarkupCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0102;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("0d819ea4-cbf2-4a94-b2a7-697cb936c78b");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EscapeMarkupCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private EscapeMarkupCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static EscapeMarkupCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in EscapeMarkupCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new EscapeMarkupCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var dte = (DTE2)Package.GetGlobalService(typeof(DTE));
+            TextSelection selection = (TextSelection)dte.ActiveDocument.Selection;
+            string text = selection.Text;
+
+            if (!string.IsNullOrEmpty(text))
+            {
+                string escaped = EscapeMarkup(text);
+                selection.Text = escaped;
+            }
+        }
+
+        private string EscapeMarkup(string input)
+        {
+            return input
+                .Replace("&", "&amp;")
+                .Replace("<", "&lt;")
+                .Replace(">", "&gt;");
+        }
+    }
+}
---
+++ Macros/MarkupMacros/README.md
@@ -0,0 +1,38 @@
+﻿# MarkupMacros - Visual Studio Extension
+
+**MarkupMacros** is a Visual Studio extension that provides a collection of macros for markup editing. These macros can be assigned to keyboard shortcuts, allowing you to perform common text transformations quickly and efficiently without using the mouse.
+
+## Features
+
+- Apply superscript formatting with a single keyboard shortcut.
+- Add additional macros as needed for HTML, XML, or other markup languages.
+- Branchless and lightweight implementation for fast execution within the editor.
+
+## Building the Extension
+
+1. Open the solution `MarkupMacros.sln` in Visual Studio.
+2. Ensure the target framework is set to **.NET Framework 4.7.2** (or the framework specified in the project).
+3. Build the project in **Debug** or **Release** mode.
+4. After a successful build, the `.vsix` file will be generated in:
+_bin\Debug\MarkupMacros.vsix_<br/>
+or<br/>
+_bin\Release\MarkupMacros.vsix_
+
+## Installing the Extension Locally
+
+1. Locate the generated `.vsix` file in the `bin` folder.
+2. Double-click the `.vsix` file to launch the Visual Studio VSIX Installer.
+3. Select the version of Visual Studio where you want to install the extension.
+4. Click **Install** and wait for the installation to complete.
+5. Restart Visual Studio if necessary to activate the commands.
+
+> Tip: During development, you can press **F5** in Visual Studio to launch an experimental instance with the extension loaded without permanently installing it.
+
+## Assigning Keyboard Shortcuts
+
+1. Open Visual Studio and go to **Tools → Options → Environment → Keyboard**.
+2. In the **"Show commands containing"** field, search for the macro command, e.g., `Tools.SurroundwithSuperscript`.
+3. Select the command and assign your preferred keyboard shortcut.
+4. Click **Assign** and then **OK**.
+
+You can now use your keyboard shortcut to execute the macro directly in the editor.
