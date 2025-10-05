c5691b1c3dcc769cd142e6e657cd9d50126b136d
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-10-05 21:12:00 +0200

    added 1518

---
+++ Problems/1518. Water Bottles/Program.cs
@@ -0,0 +1,12 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+Case
+    .CreateCase(
+        (9, 3),
+        13)
+    .CreateCase(
+        (15, 4),
+        19)
+    .Detect()
+    .Run();
---
+++ Problems/1518. Water Bottles/Solution.cs
@@ -0,0 +1,23 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1518.Water_Bottles;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int NumWaterBottles(int numBottles, int numExchange)
+    {
+        int result = numBottles;
+        int carry = 0;
+        do
+        {
+            carry = numBottles % numExchange;
+            numBottles = numBottles / numExchange;
+            result += numBottles;
+            numBottles += carry;
+        }
+        while (numBottles >= numExchange);
+
+        return result;
+    }
+}
