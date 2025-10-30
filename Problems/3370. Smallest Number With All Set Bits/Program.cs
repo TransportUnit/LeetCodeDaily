b63c4e7681ef2d619af9d5a0693605dc9c7ae0c9
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-10-30 23:37:04 +0100

    added 3370

---
+++ Problems/3370. Smallest Number With All Set Bits/Program.cs
@@ -0,0 +1,33 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+string cases =
+"""
+554
+1023
+
+105
+127
+
+39
+63
+
+103
+127
+
+122
+127
+
+22
+31
+
+31
+31
+""";
+
+cases
+    .ParseCases<int, int>()
+    .Detect()
+    .Run()
+    .Detect(1)
+    .Run();
---
+++ Problems/3370. Smallest Number With All Set Bits/Solution.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _3370.Smallest_Number_With_All_Set_Bits;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int SmallestNumber(int n)
+    {
+        return
+            (((n & 0b_00000000001) >> 0) * 0b_00000000001) |
+            (((n & 0b_00000000010) >> 1) * 0b_00000000011) |
+            (((n & 0b_00000000100) >> 2) * 0b_00000000111) |
+            (((n & 0b_00000001000) >> 3) * 0b_00000001111) |
+            (((n & 0b_00000010000) >> 4) * 0b_00000011111) |
+            (((n & 0b_00000100000) >> 5) * 0b_00000111111) |
+            (((n & 0b_00001000000) >> 6) * 0b_00001111111) |
+            (((n & 0b_00010000000) >> 7) * 0b_00011111111) |
+            (((n & 0b_00100000000) >> 8) * 0b_00111111111) |
+            (((n & 0b_01000000000) >> 9) * 0b_01111111111) |
+            (((n & 0b_10000000000) >> 10) * 0b_11111111111);
+    }
+
+    [ResultGenerator(ApproachIndex = 1)]
+    public int SmallestNumberFastest(int n)
+    {
+        // Find the position of the most significant bit
+        // 31 - LeadingZeroCount gives us the MSB position
+        int msb = 31 - System.Numerics.BitOperations.LeadingZeroCount((uint)n);
+
+        // Create a number with (msb+1) bits all set to 1
+        int result = (1 << (msb + 1)) - 1;
+
+        return result;
+    }
+}
