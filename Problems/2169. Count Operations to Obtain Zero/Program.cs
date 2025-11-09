5f64ff72b0338fb3b0a6df492a4d2ef59e0ebec0
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-11-09 20:49:43 +0100

    added 2169

---
+++ Problems/2169. Count Operations to Obtain Zero/Program.cs
@@ -0,0 +1,42 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+var cases =
+"""
+234
+1
+234
+
+3451
+56786
+50
+
+436
+34567
+100
+
+32556
+8899
+37
+
+0
+100000
+0
+
+456
+2356
+11
+
+10360
+45754
+43
+
+77772
+6786
+32
+""";
+
+cases
+    .ParseCases<(int, int), int>()
+    .Detect()
+    .Run();
---
+++ Problems/2169. Count Operations to Obtain Zero/Solution.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2169.Count_Operations_to_Obtain_Zero;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int CountOperations(int num1, int num2)
+    {
+        var operations = 0;
+
+        while (num2 != 0 && num1 != 0)
+        {
+            if (num1 < num2)
+            {
+                var tmp = num1;
+                num1 = num2;
+                num2 = tmp;
+            }
+
+            operations += num1 / num2;
+            num1 = num1 % num2;
+        }
+
+        return operations;
+    }
+}
