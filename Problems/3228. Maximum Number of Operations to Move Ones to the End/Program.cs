0bb89c9c3ef01a7672b3b5dd8a65bfeef9a1e32b
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-11-13 22:18:25 +0100

    added 3228

---
+++ Problems/3228. Maximum Number of Operations to Move Ones to the End/Program.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+string cases =
+"""
+1001101
+4
+
+00111
+0
+
+001110
+3
+
+1001101
+4
+
+00111
+0
+
+110
+2
+
+0011010101100
+15
+
+1010101010100000
+21
+""";
+
+cases
+    .ParseCases<string, int>()
+    .Detect()
+    .Run()
+    .Detect(1)
+    .Run();
---
+++ Problems/3228. Maximum Number of Operations to Move Ones to the End/Solution.cs
@@ -0,0 +1,44 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _3228.Maximum_Number_of_Operations_to_Move_Ones_to_the_End;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int MaxOperations(string s)
+    {
+        int ones = 0;
+        int result = 0;
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            ones += s[i - 1] - 48;
+
+            result += ones * (49 - s[i]) * ((s[i] - 48) ^ (s[i - 1] - 48));
+        }
+
+        return result;
+    }
+
+    [ResultGenerator(ApproachIndex = 1)]
+    public int MaxOperationsComprehensive(string s)
+    {
+        int ones = 0;
+        int result = 0;
+        int last = s[0];
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            ones += last - 48;
+
+            if (s[i] != last && s[i] == 48)
+            {
+                result += ones;
+            }
+
+            last = s[i];
+        }
+
+        return result;
+    }
+}
