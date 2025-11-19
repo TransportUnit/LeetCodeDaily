3d572dd8c69affa06dcb5a743241ea8ec3e2694a
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-11-19 14:41:42 +0100

    added 2154

---
+++ Problems/2154. Keep Multiplying Found Values by Two/Program.cs
@@ -0,0 +1,18 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+string cases =
+"""
+[5,3,6,1,12, 24]
+3
+48
+
+[2,7,9]
+4
+4
+""";
+
+cases
+    .ParseCases<(int[], int), int>()
+    .Detect()
+    .Run();
---
+++ Problems/2154. Keep Multiplying Found Values by Two/Solution.cs
@@ -0,0 +1,42 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2154.Keep_Multiplying_Found_Values_by_Two;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int FindFinalValue(int[] nums, int original)
+    {
+        // Idea:
+        // use a bitmask to track factors that match powers of two
+
+        ulong freq = 0;
+        ulong factor = 0;
+
+        foreach (var num in nums)
+        {
+            if (num % original == 0)
+            {
+                factor = (ulong)(num / original);
+
+                if ((factor & (factor - 1)) == 0)
+                {
+                    freq |= factor;
+                }
+            }
+        }
+
+        // Then iterate over the found factors and calculate the maximum 'original' value
+
+        int bit = 0;
+        while (bit <= 63)
+        {
+            if ((freq & (1UL << bit++)) == 0)
+                return original;
+
+            original *= 2;
+        }
+
+        return original;
+    }
+}
