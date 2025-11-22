60e79732f753fbc94b0465d732f3c8dd286bae02
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-11-22 16:14:40 +0100

    added 3190

---
+++ Problems/3190. Find Minimum Operations to Make All Elements Divisible by Three/Program.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+string cases =
+"""
+[1,2,3,4]
+3
+
+[3,6,9]
+0
+""";
+
+cases
+    .ParseCases<int[], int>()
+    .Detect()
+    .Run();
---
+++ Problems/3190. Find Minimum Operations to Make All Elements Divisible by Three/Solution.cs
@@ -0,0 +1,21 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _3190.Find_Minimum_Operations_to_Make_All_Elements_Divisible_by_Three;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int MinimumOperations(int[] nums)
+    {
+        //return nums.Sum(x => x % 3 == 0 ? 0 : 1/*Math.Min(3 - (x % 3), x % 3)*/);
+
+        int result = 0;
+
+        foreach (var num in nums)
+        {
+            result += ((num % 3) >> 1) | ((num % 3) & 1);
+        }
+
+        return result;
+    }
+}
