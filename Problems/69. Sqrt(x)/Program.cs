475bb9f45bdd8b862da61339c521f4db95651bd7
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-11-25 00:40:24 +0100

    added 69

---
+++ Problems/69. Sqrt(x)/Program.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+
+string cases =
+"""
+4
+2
+
+8
+2
+
+1
+1
+
+0
+0
+
+2147395600
+46340
+
+69
+8
+
+2147395599
+46339
+""";
+
+cases
+    .ParseCases<int, int>()
+    .Detect()
+    .Run()
+    .Detect(1)
+    .Run()
+    .Detect(2)
+    .Run();
---
+++ Problems/69. Sqrt(x)/Solution.cs
@@ -0,0 +1,78 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _69.Sqrt_x_;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int MySqrt(int x)
+    {
+        int left = 0;
+        int right = x;
+
+        while (left <= right)
+        {
+            long mid = left + (right - left) / 2;
+
+            if (mid * mid == x)
+            {
+                return (int)mid;
+            }
+            if (mid * mid > x)
+            {
+                right = (int)mid - 1;
+            }
+            else
+            {
+                left = (int)mid + 1;
+            }
+        }
+
+        return right;
+    }
+
+    [ResultGenerator(ApproachIndex = 1)]
+    public int MyOtherSqrt(int x)
+    {
+        if (x == 0)
+            return 0;
+
+        int bitlen = 32 - int.LeadingZeroCount(x);
+        double x_k = 1 << ((bitlen + 1) / 2);
+
+        double tmp = double.MinValue;
+
+        while(Math.Abs(x_k - tmp) >= 0.5)
+        {
+            tmp = x_k;
+            x_k = 0.5 * (x_k + (x/x_k));
+        }
+
+        return (int)x_k;
+    }
+
+    [ResultGenerator(ApproachIndex = 2)]
+    public int AISqrt(int n)
+    {
+        if (n < 2)
+            return n;
+
+        // 2^{ceil(bitlen/2)}
+        int bitlen = 32 - int.LeadingZeroCount(n);
+        int x = 1 << ((bitlen + 1) / 2);
+
+        while (true)
+        {
+            int y = (x + n / x) / 2;
+            if (y >= x)
+                break;
+            x = y;
+        }
+
+        // correction (1-2 steps max)
+        while ((long)x * x > n) x--;
+        while ((long)(x + 1) * (x + 1) <= n) x++;
+
+        return x;
+    }
+}
