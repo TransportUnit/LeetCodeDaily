ad8ca6bf33e87b5c8a204d9b7a9b8e3988f0687a
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-11-24 22:02:49 +0100

    added 1018

---
+++ Problems/1018. Binary Prefix Divisible By 5/Solution.cs
@@ -0,0 +1,22 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1018.Binary_Prefix_Divisible_By_5;
+
+public class Solution
+{
+    [ResultGenerator]
+    public IList<bool> PrefixesDivBy5(int[] nums)
+    {
+        int mod = 0;
+        bool[] result = new bool[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            mod = ((mod << 1) + nums[i]) % 5;
+            result[i] = mod == 0;
+        }
+
+        // bool[] implements IList<bool> (??), no cast necessary
+        return result;
+    }
+}
