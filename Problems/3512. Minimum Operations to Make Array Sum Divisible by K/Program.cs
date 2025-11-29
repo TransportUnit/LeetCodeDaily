4f1b642f4efd9488db83f5704fddb3bb22e854c6
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-11-29 23:38:11 +0100

    added 3512

---
+++ Problems/3512. Minimum Operations to Make Array Sum Divisible by K/Program.cs
@@ -0,0 +1,22 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+string cases =
+"""
+[3,9,7]
+5
+4
+
+[4,1,3]
+4
+0
+
+[3,2]
+6
+5
+""";
+
+cases
+    .ParseCases<(int[], int), int>()
+    .Detect()
+    .Run();
---
+++ Problems/3512. Minimum Operations to Make Array Sum Divisible by K/Solution.cs
@@ -0,0 +1,13 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _3512.Minimum_Operations_to_Make_Array_Sum_Divisible_by_K;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int MinOperations(int[] nums, int k)
+    {
+        var sum = nums.Sum();
+        return sum % k;
+    }
+}
