dcc98f45bf1db24f2f43baedec782d70e8e2821b
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-12-08 11:28:56 +0100

    added 1523

---
+++ Problems/1523. Count Odd Numbers in an Interval Range/Program.cs
@@ -0,0 +1,38 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+string cases =
+"""
+3
+7
+3
+
+8
+10
+1
+
+0
+0
+0
+
+1
+1
+1
+
+0
+50
+25
+
+0
+51
+26
+
+0
+999999999
+500000000
+""";
+
+cases
+    .ParseCases<(int, int), int>()
+    .Detect()
+    .Run();
---
+++ Problems/1523. Count Odd Numbers in an Interval Range/Solution.cs
@@ -0,0 +1,19 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1523.Count_Odd_Numbers_in_an_Interval_Range;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int CountOdds(int low, int high)
+    {
+        return
+            (high - low + 1) / 2 +
+            (high % 2 + low % 2) / 2;
+
+
+        // Explanation:
+        // (result - 1) <= ((high - low + 1) / 2)  <= result
+        // Add '1' to ((high - low + 1) / 2) when both 'low' and 'high' have parity = 1
+    }
+}
