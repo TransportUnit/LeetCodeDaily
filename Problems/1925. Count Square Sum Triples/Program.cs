ce115268288b5a41a3107faafa2070c4abcba67f
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-12-08 12:09:02 +0100

    added 1925

---
+++ Problems/1925. Count Square Sum Triples/Program.cs
@@ -0,0 +1,34 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+string cases =
+"""
+1
+0
+
+150
+178
+
+249
+324
+
+19
+10
+
+193
+238
+
+148
+172
+
+239
+312
+
+250
+330
+""";
+
+cases
+    .ParseCases<int, int>()
+    .Detect()
+    .Run();
