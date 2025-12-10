13f7795c89d45d05cdb5e1d6d526d6f4e880e4b6
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-12-10 17:23:40 +0100

    added 3577

---
+++ Problems/3577. Count the Number of Computer Unlocking Permutations/Solution.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _3577.Count_the_Number_of_Computer_Unlocking_Permutations;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int CountPermutations(int[] complexity)
+    {
+        // wtf?
+
+        if (complexity.Skip(1).Any(x => x <= complexity[0]))
+            return 0;
+
+        var n = complexity.Length - 1;
+
+        long result = 1;
+
+        while (n > 0)
+        {
+            result = (result * n) % 1_000_000_007;
+            n--;
+        }
+
+        return (int)result;
+    }
+}
