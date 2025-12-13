668bd028ed4966d93e81617d9d8d2185cba57a94
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-12-13 01:03:14 +0100

    added 3433

---
+++ Problems/3433. Count Mentions Per User/Program.cs
@@ -0,0 +1,42 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+Case
+    .CreateCase(
+        (3,
+        "[[MESSAGE,5,id0 id1],[OFFLINE,10,0],[OFFLINE,15,1],[MESSAGE,20,ALL],[OFFLINE,30,2],[MESSAGE,40,HERE]]".ParseThisFuckingShit()),
+        "[2,2,1]".ParseArray<int>())
+    .CreateCase(
+        (5, "[[MESSAGE,96,id0],[MESSAGE,98,ALL],[OFFLINE,22,0],[MESSAGE,25,id0],[OFFLINE,40,4],[OFFLINE,54,1],[MESSAGE,41,id2 id2 id1 id4 id4],[OFFLINE,85,0],[MESSAGE,86,HERE]]".ParseThisFuckingShit()),
+        "[3,2,4,2,3]".ParseArray<int>())
+    .CreateCase(
+        (3, "[[MESSAGE,1,ALL],[OFFLINE,66,1],[MESSAGE,66,HERE],[OFFLINE,5,1]]".ParseThisFuckingShit()),
+        "[2,1,2]".ParseArray<int>())
+    .CreateCase(
+        (3, "[[MESSAGE,2,HERE],[OFFLINE,2,1],[OFFLINE,1,0],[MESSAGE,61,HERE]]".ParseThisFuckingShit()),
+        "[1,0,2]".ParseArray<int>())
+    .CreateCase(
+        (3, "[[MESSAGE,1,id0 id1],[MESSAGE,5,id2],[MESSAGE,6,ALL],[OFFLINE,5,2]]".ParseThisFuckingShit()),
+        "[2,2,2]".ParseArray<int>())
+    .CreateCase(
+        (2, "[[MESSAGE,70,HERE],[OFFLINE,10,0],[OFFLINE,71,0]]".ParseThisFuckingShit()),
+        "[1,1]".ParseArray<int>())
+    .DetectAndRun();
+
+public static class CountMentionsPerUserExtensions
+{
+    public static IList<IList<string>> ParseThisFuckingShit(this string fuckingInput)
+    {
+        // Getting tired of this shit
+        var fuckingMoron = new List<IList<string>>();
+        var fuckingSplit = fuckingInput.Split("],");
+
+        foreach (var fuckhead in fuckingSplit)
+        {
+            var motherFucker = fuckhead.TrimStart('[').TrimEnd(']').Split(',');
+            fuckingMoron.Add(motherFucker);
+        }
+
+        return fuckingMoron;
+    }
+}
---
+++ Problems/3433. Count Mentions Per User/Solution.cs
@@ -0,0 +1,80 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _3433.Count_Mentions_Per_User;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int[] CountMentions(int numberOfUsers, IList<IList<string>> events)
+    {
+        int[] mentions = new int[numberOfUsers];
+
+        var grouped =
+            events
+                .OrderByDescending(x => x[0][0])
+                .GroupBy(x => int.Parse(x[1]))
+                .OrderBy(x => x.Key)
+                .ToList();
+
+        Span<int> offlineAt = stackalloc int[numberOfUsers];
+
+        for (int i = 0; i < numberOfUsers; i++)
+        {
+            offlineAt[i] = int.MinValue;
+        }
+
+        int allMentioned = 0;
+
+        foreach (var group in grouped)
+        {
+            var time = group.Key;
+
+            foreach (var @event in group)
+            {
+                if (@event[0] == "OFFLINE")
+                {
+                    foreach (var user in @event[2].Split(" "))
+                    {
+                        offlineAt[int.Parse(user)] = time;
+                    }
+                }
+                else if (@event[0] == "MESSAGE")
+                {
+                    if (@event[2] == "ALL")
+                    {
+                        allMentioned++;
+                        continue;
+                    }
+
+                    if (@event[2] == "HERE")
+                    {
+                        for (int i = 0; i < numberOfUsers; i++)
+                        {
+                            if (time >= offlineAt[i] + 60)
+                            {
+                                mentions[i]++;
+                            }
+                        }
+
+                        continue;
+                    }
+
+                    foreach (var user in @event[2].Replace("id", "").Split(" "))
+                    {
+                        mentions[int.Parse(user)]++;
+                    }
+                }
+            }
+        }
+
+        if (allMentioned > 0)
+        {
+            for (int i = 0; i < numberOfUsers; i++)
+            {
+                mentions[i] += allMentioned;
+            }
+        }
+
+        return mentions;
+    }
+}
