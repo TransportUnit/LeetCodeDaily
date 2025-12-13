61d9a45ac6ac29741c024e0a9dbf7ad18eb845a4
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2025-12-13 01:40:49 +0100

    added 3606

---
+++ Problems/3606. Coupon Code Validator/Program.cs
@@ -0,0 +1,33 @@
+﻿using _3606.Coupon_Code_Validator;
+using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+string cases =
+"""
+["SAVE20","","PHARMA5","SAVE@20"]
+["restaurant","grocery","pharmacy","restaurant"]
+[true,true,true,true]
+["PHARMA5","SAVE20"]
+
+["GROCERY15","ELECTRONICS_50","DISCOUNT10"]
+["grocery","electronics","invalid"]
+[false,true,true]
+["ELECTRONICS_50"]
+
+["SAVE50","SAVE30","PHARMA5","SAVE@20"]
+["restaurant","restaurant","pharmacy","restaurant"]
+[true,true,true,true]
+["PHARMA5","SAVE30","SAVE50"]
+
+["MI","b_"]
+["pharmacy","pharmacy"]
+[true,true]
+["MI","b_"]
+""";
+
+var solutionInstance = new Solution();
+Case.SetResultGenerator<(string[], string[], bool[]), string[]>((input) => solutionInstance.ValidateCoupons(input.Item1, input.Item2, input.Item3).ToArray());
+
+cases
+    .ParseCases<(string[], string[], bool[]), string[]>()
+    .Run();
---
+++ Problems/3606. Coupon Code Validator/Solution.cs
@@ -0,0 +1,43 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _3606.Coupon_Code_Validator;
+
+public class Solution
+{
+    [ResultGenerator]
+    public IList<string> ValidateCoupons(
+        string[] code,
+        string[] businessLine,
+        bool[] isActive
+    )
+    {
+        return
+            code
+                .Select((c, i) => new Coupon(c, businessLine[i], isActive[i]))
+                .Where(c => c.IsValid)
+                .OrderBy(c => c.BusinessLine[0])
+                // lexico-what? is that a country
+                .ThenBy(c => c.Code, StringComparer.Ordinal)
+                .Select(x => x.Code)
+                .ToArray();
+    }
+
+    public readonly record struct Coupon(string Code, string BusinessLine, bool IsActive)
+    {
+        public bool IsValid => IsActive && BusinessLineValid && CodeValid;
+
+        public bool BusinessLineValid =>
+            BusinessLine == "electronics" ||
+            BusinessLine == "grocery" ||
+            BusinessLine == "pharmacy" ||
+            BusinessLine == "restaurant";
+
+        public bool CodeValid =>
+            Code.Length > 0 &&
+            Code.All(c =>
+                ('a' <= c && c <= 'z') ||
+                ('A' <= c && c <= 'Z') ||
+                ('0' <= c && c <= '9') ||
+                c == '_');
+    }
+}
