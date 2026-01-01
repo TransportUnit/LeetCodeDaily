4c08ba60708ca0c7be93e9a221787ea2fee373d5
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-01-01 03:06:40 +0100

    added 1351

---
+++ Problems/1351. Count Negative Numbers in a Sorted Matrix/Program.cs
@@ -0,0 +1,15 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+string cases =
+"""
+[[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+8
+
+[[3,2],[1,0]]
+0
+""";
+
+cases
+    .ParseCases<int[][], int>()
+    .DetectAndRun();
---
+++ Problems/1351. Count Negative Numbers in a Sorted Matrix/Solution.cs
@@ -0,0 +1,29 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1351.Count_Negative_Numbers_in_a_Sorted_Matrix;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int CountNegatives(int[][] grid)
+    {
+        int result = 0;
+        int i = grid.Length - 1;
+        int j = 0;
+
+        while (i >= 0 && j < grid[0].Length)
+        {
+            if (grid[i][j] < 0)
+            {
+                result += grid[0].Length - j;
+                i--;
+            }
+            else
+            {
+                j++;
+            }
+        }
+
+        return result;
+    }
+}
