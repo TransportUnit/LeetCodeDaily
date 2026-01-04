75a4bf698474ff78dff8e49cb659a8985d61c44e
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-01-05 00:59:05 +0100

    added 1411

---
+++ Problems/1411. Number of Ways to Paint N × 3 Grid/Program.cs
@@ -0,0 +1,33 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+string cases =
+"""
+1
+12
+
+2
+54
+
+3
+246
+
+4
+1122
+
+5
+5118
+
+623
+125280856
+
+1134
+282527091
+
+5000
+30228214
+""";
+
+cases
+    .ParseCases<int, int>()
+    .DetectAndRun();
---
+++ Problems/1411. Number of Ways to Paint N × 3 Grid/Solution.cs
@@ -0,0 +1,23 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1411.Number_of_Ways_to_Paint_N___3_Grid;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int NumOfWays(int n)
+    {
+        long z = 6;
+        long d = 6;
+        while (n-- > 1)
+        {
+            long zn = (3 * z + 2 * d) % 1_000_000_007;
+            long dn = (2 * z + 2 * d) % 1_000_000_007;
+
+            z = zn;
+            d = dn;
+        }
+
+        return (int)((z + d) % 1_000_000_007);
+    }
+}
