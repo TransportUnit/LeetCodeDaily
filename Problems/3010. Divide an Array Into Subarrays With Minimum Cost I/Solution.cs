22d7cf90f44d58977d95f2f2692e8d5d290afd47
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-02-02 17:33:48 +0100

    added 3010

---
+++ Problems/3010. Divide an Array Into Subarrays With Minimum Cost I/Solution.cs
@@ -0,0 +1,15 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _3010.Divide_an_Array_Into_Subarrays_With_Minimum_Cost_I;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int MinimumCost(int[] nums)
+    {
+        // the first element is always part of the resulting sum
+        // the other two indices can be chosen as the two smallest elements from the rest of the array
+
+        return nums[0] + nums.Skip(1).OrderBy(x => x).Take(2).Sum();
+    }
+}
