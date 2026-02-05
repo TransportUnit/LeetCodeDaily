92d9b0e934efba5b94d0a46b176ee561bd67c82c
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-02-05 12:12:50 +0100

    added 3379

---
+++ Problems/3379. Transformed Array/Solution.cs
@@ -0,0 +1,22 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _3379.Transformed_Array;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int[] ConstructTransformedArray(int[] nums)
+    {
+        int n = nums.Length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            // add n * 100 to i and nums[i] before performing modulo n for left shift / avoids negative modulo
+            int index = ((n * 100) + i + nums[i]) % n;
+            result[i] = nums[index];
+        }
+
+        return result;
+    }
+}
