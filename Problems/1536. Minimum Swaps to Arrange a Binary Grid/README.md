bfded4938b29085dee15b1154cb81858fe314581
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-03-02 11:42:47 +0100

    added 1536

---
+++ Problems/1536. Minimum Swaps to Arrange a Binary Grid/README.md
@@ -0,0 +1,62 @@
+﻿# 1536. Minimum Swaps to Arrange a Binary Grid
+
+Given an `n x n` binary `grid`, in one step you can choose two **adjacent rows** of the grid and swap them.
+
+A grid is said to be **valid** if all the cells above the main diagonal are **zeros**.
+
+Return _the minimum number of steps_ needed to make the grid valid, or **\-1** if the grid cannot be valid.
+
+The main diagonal of a grid is the diagonal that starts at cell `(1, 1)` and ends at cell `(n, n)`.
+
+<br/>
+
+# **Example 1:**
+
+![](ex1.jpg)
+
+**Input:** grid = \[\[0,0,1\],\[1,1,0\],\[1,0,0\]\]
+
+**Output:** 3
+
+# **Example 2:**
+
+![](ex2.jpg)
+
+**Input:** grid = \[\[0,1,1,0\],\[0,1,1,0\],\[0,1,1,0\],\[0,1,1,0\]\]
+
+**Output:** -1
+
+**Explanation:** All rows are similar, swaps have no effect on the grid.
+
+# **Example 3:**
+
+![](ex3.jpg)
+
+**Input:** grid = \[\[1,0,0\],\[1,1,0\],\[1,1,1\]\]
+
+**Output:** 0
+
+<br/>
+
+# **Constraints:**
+
+*   `n == grid.length` `== grid[i].length`
+*   `1 <= n <= 200`
+*   `grid[i][j]` is either `0` or `1`
+
+<br/>
+
+<details>
+<summary>Hint 1</summary>
+<p>For each row of the grid calculate the most right 1 in the grid in the array maxRight.</p>
+</details>
+
+<details>
+<summary>Hint 2</summary>
+<p>To check if there exist answer, sort maxRight and check if maxRight[i] ≤ i for all possible i's.</p>
+</details>
+
+<details>
+<summary>Hint 3</summary>
+<p>If there exist an answer, simulate the swaps.</p>
+</details>
---
+++ Problems/1536. Minimum Swaps to Arrange a Binary Grid/Solution.cs
@@ -0,0 +1,90 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1536.Minimum_Swaps_to_Arrange_a_Binary_Grid;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int MinSwaps(int[][] grid)
+    {
+        int n = grid.Length;
+
+        // handle special cases
+        if (n <= 1)
+            return 0;
+
+        Span<int> maxRight = stackalloc int[n];
+        Span<int> maxRightFreq = stackalloc int[n];
+
+        // recording the rightmost 1 in each row and its frequency
+        for (int i = 0; i < n; i++)
+        {
+            int j = n - 1;
+
+            for (; j >= 0; j--)
+            {
+                if (grid[i][j] == 1)
+                {
+                    maxRightFreq[j]++;
+                    maxRight[i] = j;
+                    break;
+                }
+            }
+
+            // if there is no 1 in the row, we can treat it as if there is a 1 at index 0
+            if (j < 0)
+            {
+                maxRightFreq[0]++;
+            }
+        }
+
+
+        // checking feasibility 
+        for (int i = 0; i < n; i++)
+        {
+            if (maxRightFreq[i] <= 0)
+            {
+                int j = i - 1;
+
+                while (j >= 0 && maxRightFreq[j] <= 0)
+                {
+                    j--;
+                }
+
+                if (j < 0)
+                {
+                    return -1;
+                }
+
+                maxRightFreq[j]--;
+                continue;
+            }
+
+            maxRightFreq[i]--;
+        }
+
+        int result = 0;
+
+        // simulating and counting swap operations
+        for (int i = 0; i < n; i++)
+        {
+            if (maxRight[i] > i)
+            {
+                int j = i + 1;
+
+                while (j < n && maxRight[j] > i)
+                {
+                    j++;
+                }
+
+                for (; j > i; j--)
+                {
+                    (maxRight[j], maxRight[j - 1]) = (maxRight[j - 1], maxRight[j]);
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
