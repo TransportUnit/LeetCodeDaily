8631751403649aca5908290a1cc5c867184434f1
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-03-05 10:04:30 +0100

    added 1758

---
+++ Problems/1758. Minimum Changes To Make Alternating Binary String/Program.cs
@@ -0,0 +1,19 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+
+"""
+0100
+1
+
+10
+0
+
+1111
+2
+
+010011110001100110010101000101010100100101001100010010101111000
+23
+"""
+.ParseCases<string, int>()
+.DetectAndRun();
---
+++ Problems/1758. Minimum Changes To Make Alternating Binary String/Solution.cs
@@ -0,0 +1,42 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1758.Minimum_Changes_To_Make_Alternating_Binary_String;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int MinOperations(string s)
+    {
+        int mismatchesStartingWithZero = 0;
+        int mismatchesStartingWithOne = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            // approach three
+            mismatchesStartingWithZero += (1 - (i & 1)) * (s[i] - '0') + (i & 1) * ('1' - s[i]);
+
+
+            /*
+            // approach two
+
+            int isOne = s[i] - '0';
+            int isZero = 1 - isOne; // '1' - s[i]
+            int odd = i & 1;
+            int even = 1 - odd; //(1 - (i & 1));
+
+            mismatchesStartingWithZero += even * isOne + odd * isZero;
+            mismatchesStartingWithOne += even * isZero + odd * isOne;
+            */
+
+
+            // approach one
+            // mismatchesStartingWithZero += (1 - (i & 1)) * (s[i] - '0') + (i & 1) * ('1' - s[i]);
+            // mismatchesStartingWithOne += (1 - (i & 1)) * ('1' - s[i]) + (i & 1) * (s[i] - '0');
+        }
+
+        // approach three
+        mismatchesStartingWithOne = s.Length - mismatchesStartingWithZero;
+
+        return Math.Min(mismatchesStartingWithZero, mismatchesStartingWithOne);
+    }
+}
