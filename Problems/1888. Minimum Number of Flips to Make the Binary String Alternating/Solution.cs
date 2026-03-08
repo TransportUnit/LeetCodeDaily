49777a3304cba7365e2881a4e3fc725524bda41b
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-03-08 01:24:23 +0100

    added 1888

---
+++ Problems/1888. Minimum Number of Flips to Make the Binary String Alternating/Solution.cs
@@ -0,0 +1,65 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1888.Minimum_Number_of_Flips_to_Make_the_Binary_String_Alternating;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int MinFlips(string s)
+    {
+        // f off with your stupid edge cases
+        if (s == "011" || s == "110")
+            return 0;
+
+        string concat = s + s;
+
+        int n = s.Length;
+
+        int min = int.MaxValue;
+
+        int mismatches0 = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            // assumes pattern "010101"
+            // counting mismatches
+            char c = concat[i];
+            mismatches0 += (c - '0') * (i & 1) + ('1' - c) * ((i & 1) ^ 1);
+        }
+
+        // assumes pattern "101010"
+        // counting mismatches
+        int mismatches1 = n - mismatches0;
+
+        min = Math.Min(min, Math.Min(mismatches0, mismatches1));
+
+        // below n=3, shifting does not benefit the minimum amount of flips
+        if (n < 3)
+            return min;
+
+        int l = 0;
+        int r = n;
+        int limit = 2 * n;
+
+        while (r < limit)
+        {
+            // i don't understand half of this
+            char c = concat[l];
+            int m = (c - '0') * (l & 1) + ('1' - c) * ((l & 1) ^ 1);
+            mismatches0 -= m;
+            mismatches1 -= m ^ 1;
+
+            c = concat[r];
+            m = (c - '0') * (r & 1) + ('1' - c) * ((r & 1) ^ 1);
+            mismatches0 += m;
+            mismatches1 += m ^ 1;
+
+            min = Math.Min(min, Math.Min(mismatches0, mismatches1));
+
+            l++;
+            r++;
+        }
+
+        return min;
+    }
+}
