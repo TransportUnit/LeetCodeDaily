53ea362ee0151b65fc72c70498d3972bb8a3044b
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-03-11 13:26:34 +0100

    added 1009

---
+++ Problems/1009. Complement of Base 10 Integer/Program.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+"""
+0
+1
+
+2
+1
+
+999999999
+73741824
+
+8
+7
+
+1010101
+38474
+
+7
+0
+
+1000000000
+73741823
+"""
+.ParseCases<int, int>()
+.DetectAndRun();
---
+++ Problems/1009. Complement of Base 10 Integer/Solution.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeDaily.Core;
+using System.Numerics;
+
+namespace _1009.Complement_of_Base_10_Integer;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int BitwiseComplement(int n)
+    {
+        if (n == 0)
+            return 1;
+
+        return n ^ (-1 >>> BitOperations.LeadingZeroCount((uint)n));
+    }
+}
