07bb9d320356f5d9ef2c9efb101ca722855a97f9
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-03-26 21:53:35 +0100

    rewrote 706 to support string input test cases

--- Problems/706. Design HashMap/Program.cs
+++ Problems/706. Design HashMap/Program.cs
@@ -1,35 +1,21 @@
-﻿using _706._Design_HashMap;
-using LeetCodeDaily.Core;
+﻿using LeetCodeDaily.Extensions;
 
-ResultGeneratorAttribute.Detect();
+"""
+["MyHashMap","put","put","get","get","put","get","remove","get"]
+[[],[1,1],[2,2],[1],[3],[2,1],[2],[2],[2]]
+[null,null,null,1,-1,null,1,null,-1]
 
-Case
-    .CreateCase<Func<MyHashMap, int>, int>(
-        hashMap =>
-        {
-            hashMap.Put(1, 1);
-            hashMap.Put(2, 2);
-            return hashMap.Get(1);
-        },
-        1)
-    .CreateCase(
-        hashMap =>
-        {
-            return hashMap.Get(3);
-        },
-        -1)
-    .CreateCase(
-        hashMap =>
-        {
-            hashMap.Put(2, 1);
-            return hashMap.Get(2);
-        },
-        1)
-    .CreateCase(
-        hashMap =>
-        {
-            hashMap.Remove(2);
-            return hashMap.Get(2);
-        },
-        -1)
-    .Run();
+["MyHashMap","remove","put","put","put","put","put","put","get","put","put"]
+[[],[2],[3,11],[4,13],[15,6],[6,15],[8,8],[11,0],[11],[1,10],[12,14]]
+[null,null,null,null,null,null,null,null,0,null,null]
+
+["MyHashMap","remove","put","get","remove","remove","put","get","put","put","put","get","remove","get","remove","remove","remove","remove","put","put","remove"]
+[[],[416695],[495113,909657],[627667],[237284],[77276],[84018,624558],[340321],[823877,944894],[295210,11560],[793578,436501],[655802],[418157],[784343],[56761],[67700],[387474],[832102],[131359,624075],[729011,168329],[743575]]
+[null,null,null,-1,null,null,null,-1,null,null,null,-1,null,-1,null,null,null,null,null,null,null]
+
+["MyHashMap","put","get","put","get"]
+[[],[1000000,1],[1000000],[0,2],[0]]
+[null,null,1,null,2]
+"""
+.ParseCases<(string, string), string>()
+.DetectAndRun();
--- Problems/706. Design HashMap/Solution.cs
+++ Problems/706. Design HashMap/Solution.cs
@@ -1,19 +1,104 @@
 ﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+using System.Text;
 
 namespace _706._Design_HashMap;
 
 public class Solution
 {
-    private readonly MyHashMap _myHashMap;
-
-    public Solution()
-    {
-        _myHashMap = new MyHashMap();
-    }
-
     [ResultGenerator]
-    public int Do(Func<MyHashMap, int> action)
+    public string PerformActions(string actionsInput, string valuesInput)
     {
-        return action(_myHashMap);
+        StringBuilder sb = new();
+        sb.Append("[");
+
+        MyHashMap? hash = null;
+
+        List<Func<int?>> actions = new();
+
+        var actionsStrings = actionsInput.ParseArray<string>(removeQuotes: true);
+
+        int valuePointer = 0;
+
+        foreach (var action in actionsStrings)
+        {
+            var vals = GetValue();
+
+            switch (action.ToLower())
+            {
+                case "myhashmap":
+                    {
+                        actions.Add(() => { hash = new MyHashMap(); return null; });
+                        break;
+                    }
+                case "put":
+                    {
+                        actions.Add(() => { hash!.Put(vals[0], vals[1]); return null; });
+                        break;
+                    }
+                case "get":
+                    {
+                        actions.Add(() => { return hash!.Get(vals[0]); });
+                        break;
+                    }
+                case "remove":
+                    {
+                        actions.Add(() => { hash!.Remove(vals[0]); return null; });
+                        break;
+                    }
+            }
+        }
+
+        bool first = true;
+        foreach (var action in actions)
+        {
+            if (first)
+            {
+                first = false;
+            }
+            else
+            {
+                sb.Append(",");
+            }
+
+            int? result = action();
+            sb.Append(result?.ToString() ?? "null");
+        }
+
+        sb.Append("]");
+        return sb.ToString();
+
+        int[] GetValue()
+        {
+            string curr = "";
+            List<int> vals = new();
+
+            while (valuePointer < valuesInput.Length &&
+                   valuesInput[valuePointer] != ']')
+            {
+                if (valuesInput[valuePointer] >= '0' &&
+                    valuesInput[valuePointer] <= '9')
+                {
+                    curr += valuesInput[valuePointer];
+                }
+                else
+                {
+                    if (curr.Length > 0)
+                    {
+                        vals.Add(int.Parse(curr));
+                    }
+                    curr = "";
+                }
+
+                valuePointer++;
+            }
+            if (curr.Length > 0)
+            {
+                vals.Add(int.Parse(curr));
+            }
+            valuePointer++;
+
+            return vals.ToArray();
+        }
     }
 }
