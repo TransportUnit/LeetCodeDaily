e150cefc832073c77f7747a96fe1008b98a66c3b
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-04-08 12:18:40 +0200

    added 3653

---
+++ Problems/3653. XOR After Range Multiplication Queries I/Program.cs
@@ -0,0 +1,14 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+"""
+[1,1,1]
+[[0,2,1,4]]
+4
+
+[2,3,1,5,4]
+[[1,4,2,3],[0,2,1,2]]
+31
+"""
+.ParseCases<int[], int[][], int>()
+.DetectAndRun();
---
+++ Problems/3653. XOR After Range Multiplication Queries I/Solution.cs
@@ -0,0 +1,33 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _3653.XOR_After_Range_Multiplication_Queries_I;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int XorAfterQueries(int[] nums, int[][] queries)
+    {
+        int mod = 1_000_000_007;
+
+        foreach (var query in queries)
+        {
+            int idx = query[0];
+
+            while (idx <= query[1])
+            {
+                long newVal = ((long)nums[idx] * query[3]) % mod;
+                nums[idx] = (int)newVal;
+                idx += query[2];
+            }
+        }
+
+        int xor = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            xor ^= nums[i];
+        }
+
+        return xor;
+    }
+}
