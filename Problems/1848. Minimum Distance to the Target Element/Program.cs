22c1aba3ea36cb63c4e6333a0027ed5f2b2b5551
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-04-13 11:13:47 +0200

    added 1848

---
+++ Problems/1848. Minimum Distance to the Target Element/Program.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+"""
+[1,2,3,4,5]
+5
+3
+1
+
+[1]
+1
+0
+0
+
+[1,1,1,1,1,1,1,1,1,1]
+1
+0
+0
+
+[5,3,6]
+5
+2
+2
+"""
+.ParseCases<int[], int, int, int>()
+.DetectAndRun();
---
+++ Problems/1848. Minimum Distance to the Target Element/Solution.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _1848.Minimum_Distance_to_the_Target_Element;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int GetMinDistance(int[] nums, int target, int start)
+    {
+        if (nums[start] == target)
+            return 0;
+
+        int forward = start + 1;
+        int backward = start - 1;
+        int i = 1;
+
+        while (true)
+        {
+            if (backward >= 0 && nums[backward] == target)
+            {
+                return i;
+            }
+            backward--;
+
+            if (forward < nums.Length && nums[forward] == target)
+            {
+                return i;
+            }
+            forward++;
+
+            i++;
+        }
+
+        return 0;
+    }
+}
