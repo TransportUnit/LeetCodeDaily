4b60364f148a4f850c9b97d0f32ad8209237144b
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-04-20 13:19:19 +0200

    added 2078

---
+++ Problems/2078. Two Furthest Houses With Different Colors/Program.cs
@@ -0,0 +1,21 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+"""
+[1,1,1,6,1,1,1]
+3
+
+[1,8,3,8,3]
+4
+
+[0,1]
+1
+
+[1,6,1,1,1,1,1]
+5
+
+[1,1,1,1,1,6,1]
+5
+"""
+.ParseCases<int[], int>()
+.DetectAndRun();
---
+++ Problems/2078. Two Furthest Houses With Different Colors/Solution.cs
@@ -0,0 +1,43 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2078.Two_Furthest_Houses_With_Different_Colors;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int MaxDistance(int[] colors)
+    {
+        int n = colors.Length;
+
+        if (n <= 2)
+        {
+            return 1;
+        }
+
+        int dist = n - 1;
+
+        int leftmostColor = colors[0];
+        int rightmostColor = colors[^1];
+
+        if (leftmostColor != rightmostColor)
+        {
+            return dist;
+        }
+
+        int left = 1;
+        int right = n - 2;
+
+        while (left <= right)
+        {
+            dist--;
+
+            if (colors[left++] != rightmostColor ||
+                colors[right--] != leftmostColor)
+            {
+                return dist;
+            }
+        }
+
+        return 0;
+    }
+}
