6287d992be026a96bd51706f4735a156d319fc31
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-05-05 14:47:39 +0200

    added 48

---
+++ Problems/48. Rotate Image/Solution.cs
@@ -0,0 +1,39 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _48.Rotate_Image;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int[][] RotateWrapper(int[][] matrix)
+    {
+        Rotate(matrix);
+        return matrix;
+    }
+
+    public void Rotate(int[][] matrix)
+    {
+        var n = matrix.Length;
+
+        Span<int> cache = stackalloc int[n * n];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cache[i * n + j] = matrix[i][j];
+            }
+        }
+
+        int nSquare = n * n;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                int offset = nSquare - (n - i + j * n);
+                matrix[i][j] = cache[offset];
+            }
+        }
+    }
+}
