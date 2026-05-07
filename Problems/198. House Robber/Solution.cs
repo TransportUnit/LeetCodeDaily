e095782a568f0392fb1741f894ddffcfd1d50d35
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-05-07 19:45:46 +0200

    added 198

---
+++ Problems/198. House Robber/Solution.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _198.House_Robber;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int Rob(int[] nums)
+    {
+        int n = nums.Length;
+
+        if (n < 2)
+            return nums[0];
+
+        Span<int> dp = stackalloc int[n];
+
+        dp[0] = nums[0];
+        dp[1] = Math.Max(dp[0], nums[1]);
+
+        for (int i = 2; i < n; i++)
+        {
+            dp[i] = Math.Max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+
+        return dp[^1];
+    }
+}
