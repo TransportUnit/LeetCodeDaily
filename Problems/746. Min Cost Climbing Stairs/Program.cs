b3ab1ebc421da6e454ffa1095f0e3187120e94c2
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-05-07 19:07:08 +0200

    added 746

---
+++ Problems/746. Min Cost Climbing Stairs/Program.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+"""
+[10,15,20]
+15
+
+[1,100,1,1,1,100,1,1,100,1]
+6
+"""
+.ParseCases<int[], int>()
+.DetectAndRun();
+
+/*
+// Legacy
+Case
+    .CreateCase(
+        (2, 2),
+        4)
+    .CreateCase(
+        (6, 9),
+        15)
+    .Detect()
+    .Run();
+*/
---
+++ Problems/746. Min Cost Climbing Stairs/Solution.cs
@@ -0,0 +1,19 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _746.Min_Cost_Climbing_Stairs;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int MinCostClimbingStairs(int[] cost)
+    {
+        var dp = new int[cost.Length + 2];
+
+        for (int i = cost.Length - 1; i >= 0; i--)
+        {
+            dp[i] = cost[i] + Math.Min(dp[i + 1], dp[i + 2]);
+        }
+
+        return Math.Min(dp[0], dp[1]);
+    }
+}
