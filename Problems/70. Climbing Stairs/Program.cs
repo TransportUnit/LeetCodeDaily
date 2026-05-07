984fe40e4822a18dee350a4413efc044b3195810
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-05-07 18:40:36 +0200

    added 70

---
+++ Problems/70. Climbing Stairs/Program.cs
@@ -0,0 +1,31 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+"""
+2
+2
+
+3
+3
+
+4
+5
+
+5
+8
+"""
+.ParseCases<int, int>()
+.DetectAndRun();
+
+/*
+// Legacy
+Case
+    .CreateCase(
+        (2, 2),
+        4)
+    .CreateCase(
+        (6, 9),
+        15)
+    .Detect()
+    .Run();
+*/
---
+++ Problems/70. Climbing Stairs/Solution.cs
@@ -0,0 +1,22 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _70.Climbing_Stairs;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int ClimbStairs(int n)
+    {
+        if (n <= 1)
+            return 1;
+
+        Span<int> arr = stackalloc int[n];
+        arr[0] = 1;
+        arr[1] = 2;
+        for (int i = 2; i < n; i++)
+        {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[^1];
+    }
+}
