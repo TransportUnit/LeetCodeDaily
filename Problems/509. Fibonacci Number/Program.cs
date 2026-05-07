b82dfe5d8267cc126d87f78f6cc662b7ff0cb7b4
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-05-07 18:56:04 +0200

    added 509

---
+++ Problems/509. Fibonacci Number/Program.cs
@@ -0,0 +1,34 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+"""
+2
+1
+
+3
+2
+
+4
+3
+
+0
+0
+
+30
+832040
+"""
+.ParseCases<int, int>()
+.DetectAndRun();
+
+/*
+// Legacy
+Case
+    .CreateCase(
+        (2, 2),
+        4)
+    .CreateCase(
+        (6, 9),
+        15)
+    .Detect()
+    .Run();
+*/
---
+++ Problems/509. Fibonacci Number/Solution.cs
@@ -0,0 +1,33 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _509.Fibonacci_Number;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int Fib(int n)
+    {
+        if (n <= 0)
+            return 0;
+
+        if (n <= 2)
+            return 1;
+
+        int[] dic = new int[31];
+        return FibInt(n, dic);
+    }
+
+    private static int FibInt(int n, int[] dic)
+    {
+        if (n <= 2)
+            return 1;
+
+        if (dic[n] > 0)
+        {
+            return dic[n];
+        }
+
+        dic[n] = FibInt(n - 1, dic) + FibInt(n - 2, dic);
+        return dic[n];
+    }
+}
