9afabc2f3b806042ed7f4a88e927e20725ad08dc
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-05-11 15:51:09 +0200

    added 2553

---
+++ Problems/2553. Separate the Digits in an Array/Program.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeDaily.Extensions;
+
+"""
+[13,25,83,77]
+[1,3,2,5,8,3,7,7]
+
+[7,1,3,9]
+[7,1,3,9]
+"""
+.UnsafeRun();
+
+//.ParseCases<int[], int[]>()
+//.DetectAndRun();
+
+/*
+// Legacy
+Case
+    .CreateCase(
+        (2, 2),
+        4)
+    .CreateCase(
+        (6, 9),
+        15)
+    .Detect()
+    .Run();
+*/
---
+++ Problems/2553. Separate the Digits in an Array/Solution.cs
@@ -0,0 +1,69 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2553.Separate_the_Digits_in_an_Array;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int[] SeparateDigits(int[] nums)
+    {
+        int n = nums.Length;
+        var maxSize = n * 6;
+
+        Span<int> output = stackalloc int[maxSize];
+        int count = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            var val = nums[i];
+
+            if (val < 10)
+            {
+                output[count] = val;
+                count++;
+            }
+            else if (val < 100)
+            {
+                output[count] = val / 10;
+                output[count + 1] = val % 10;
+                count += 2;
+            }
+            else if (val < 1_000)
+            {
+                output[count] = val / 100;
+                output[count + 1] = (val / 10) % 10;
+                output[count + 2] = val % 10;
+                count += 3;
+            }
+            else if (val < 10_000)
+            {
+                output[count] = val / 1_000;
+                output[count + 1] = (val / 100) % 10;
+                output[count + 2] = (val / 10) % 10;
+                output[count + 3] = val % 10;
+                count += 4;
+            }
+            else if (val < 100_000)
+            {
+                output[count] = val / 10_000;
+                output[count + 1] = (val / 1_000) % 10;
+                output[count + 2] = (val / 100) % 10;
+                output[count + 3] = (val / 10) % 10;
+                output[count + 4] = val % 10;
+                count += 5;
+            }
+            else
+            {
+                output[count] = 1;
+                output[count + 1] = 0;
+                output[count + 2] = 0;
+                output[count + 3] = 0;
+                output[count + 4] = 0;
+                output[count + 5] = 0;
+                count += 6;
+            }
+        }
+
+        return output.Slice(0, count).ToArray();
+    }
+}
