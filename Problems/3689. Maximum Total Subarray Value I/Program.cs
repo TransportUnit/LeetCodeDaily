05fea7026c7359f1e55f5224e6c66aee3dced35c
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-06-09 10:43:53 +0200

    added 3689

---
+++ Problems/3689. Maximum Total Subarray Value I/Program.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+"""
+[1,3,2]
+2
+4
+
+[4,2,5,1]
+3
+12
+"""
+.UnsafeRun();
+
+/*
+// Legacy
+Case
+    .CreateCase(
+        (2, 2),
+        4)
+    .CreateCase(
+        (6, 9),
+        15)
+    .Detect()
+    .Run();
+*/
---
+++ Problems/3689. Maximum Total Subarray Value I/Solution.cs
@@ -0,0 +1,24 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _3689.Maximum_Total_Subarray_Value_I
+{
+    public class Solution
+    {
+        [ResultGenerator]
+        public long MaxTotalValue(int[] nums, int k)
+        {
+            int n = nums.Length;
+
+            long min = int.MaxValue;
+            long max = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                min = Math.Min(min, nums[i]);
+                max = Math.Max(max, nums[i]);
+            }
+
+            return (max - min) * (long)k;
+        }
+    }
+}
