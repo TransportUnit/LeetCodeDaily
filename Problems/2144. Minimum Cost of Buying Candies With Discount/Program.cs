959ab336195f486d1452c684b98def40b7883d2a
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-06-09 10:45:07 +0200

    added 2144

---
+++ Problems/2144. Minimum Cost of Buying Candies With Discount/Program.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+"""
+[1,2,3]
+5
+
+[6,5,7,9,2,2]
+23
+
+[5,5]
+10
+"""
+.UnsafeRun();
+
+/*
+// Legacy
+Case
+    .CreateCase(
+        (2, 2),
+        4)
+    .CreateCase(
+        (6, 9),
+        15)
+    .Detect()
+    .Run();
+*/
---
+++ Problems/2144. Minimum Cost of Buying Candies With Discount/Solution.cs
@@ -0,0 +1,33 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _2144.Minimum_Cost_of_Buying_Candies_With_Discount;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int MinimumCost(int[] cost)
+    {
+        int n = cost.Length;
+
+        Span<int> costOrdered = stackalloc int[101];
+
+        for (int i = 0; i < n; i++)
+        {
+            costOrdered[cost[i]]++;
+        }
+
+        int total = 0;
+        int j = 0;
+
+        for (int i = 100; i > 0; i--)
+        {
+            while (costOrdered[i]-- > 0)
+            {
+                total += i - (j / 2) * i;
+                j = (j + 1) % 3;
+            }
+        }
+
+        return total;
+    }
+}
