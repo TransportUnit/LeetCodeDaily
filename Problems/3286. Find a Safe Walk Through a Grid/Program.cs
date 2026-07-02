d41a7f467638e57cd12c686bae26ad4d22f841c0
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-07-02 16:07:59 +0200

    added 3286

---
+++ Problems/3286. Find a Safe Walk Through a Grid/Program.cs
@@ -0,0 +1,30 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+"""
+[[0,1,0,0,0],[0,1,0,1,0],[0,0,0,1,0]]
+1
+true
+
+[[0,1,1,0,0,0],[1,0,1,0,0,0],[0,1,1,1,0,1],[0,0,1,0,1,0]]
+3
+false
+
+[[1,1,1],[1,0,1],[1,1,1]]
+5
+true
+"""
+.UnsafeRun();
+
+/*
+// Legacy
+Case
+    .CreateCase(
+        (2, 2),
+        4)
+    .CreateCase(
+        (6, 9),
+        15)
+    .Detect()
+    .Run();
+*/
---
+++ Problems/3286. Find a Safe Walk Through a Grid/Solution.cs
@@ -0,0 +1,81 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _3286.Find_a_Safe_Walk_Through_a_Grid;
+
+public class Solution
+{
+    [ResultGenerator]
+    public bool FindSafeWalkWrapper(int[][] grid, int health)
+    {
+        return FindSafeWalk(grid, health);
+    }
+
+    public bool FindSafeWalk(IList<IList<int>> grid, int health)
+    {
+        int rows = grid.Count;
+        int cols = grid[0].Count;
+
+        if (grid[0][0] >= health)
+        {
+            return false;
+        }
+
+        int[,] dist = new int[rows, cols];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                dist[i, j] = int.MaxValue;
+            }
+        }
+
+        dist[0, 0] = grid[0][0];
+
+        LinkedList<(int x, int y)> deque = new();
+        deque.AddFirst((0, 0));
+
+        int[] dx = { -1, 1, 0, 0 };
+        int[] dy = { 0, 0, -1, 1 };
+
+        while (deque.Count > 0)
+        {
+            var current = deque.First.Value;
+            deque.RemoveFirst();
+
+            int x = current.x;
+            int y = current.y;
+
+            if (x == rows - 1 && y == cols - 1)
+            {
+                break;
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx >= 0 && nx < rows && ny >= 0 && ny < cols)
+                {
+                    int cost = grid[nx][ny];
+
+                    if (dist[x, y] + cost < dist[nx, ny])
+                    {
+                        dist[nx, ny] = dist[x, y] + cost;
+
+                        if (cost == 0)
+                        {
+                            deque.AddFirst((nx, ny));
+                        }
+                        else
+                        {
+                            deque.AddLast((nx, ny));
+                        }
+                    }
+                }
+            }
+        }
+
+        return health - dist[rows - 1, cols - 1] >= 1;
+    }
+}
