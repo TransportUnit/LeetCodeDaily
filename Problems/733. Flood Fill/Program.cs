bbe11b9cf954527bd674a76660366ef2d159961e
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-07-02 16:30:05 +0200

    added 733

---
+++ Problems/733. Flood Fill/Program.cs
@@ -0,0 +1,30 @@
+﻿using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+"""
+[[1,1,1],[1,1,0],[1,0,1]]
+1
+1
+2
+[[2,2,2],[2,2,0],[2,0,1]]
+
+[[0,0,0],[0,0,0]]
+0
+0
+0
+[[0,0,0],[0,0,0]]
+"""
+.UnsafeRun();
+
+/*
+// Legacy
+Case
+    .CreateCase(
+        (2, 2),
+        4)
+    .CreateCase(
+        (6, 9),
+        15)
+    .Detect()
+    .Run();
+*/
---
+++ Problems/733. Flood Fill/Solution.cs
@@ -0,0 +1,58 @@
+﻿using LeetCodeDaily.Core;
+
+namespace _733.Flood_Fill;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int[][] FloodFill(int[][] image, int sr, int sc, int color)
+    {
+        int startingColor = image[sr][sc];
+
+        if (color == startingColor)
+        {
+            return image;
+        }
+
+        int m = image.Length;
+        int n = image[0].Length;
+
+        int[] dx = { -1, 1, 0, 0 };
+        int[] dy = { 0, 0, -1, 1 };
+
+        Queue<(int x, int y)> queue = new();
+
+        queue.Enqueue((sr, sc));
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            int x = current.x;
+            int y = current.y;
+
+            image[x][y] = color;
+
+            for (int i = 0; i < 4; i++)
+            {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (BoundsCheck(nx, ny, m, n) &&
+                    image[nx][ny] == startingColor)
+                {
+                    queue.Enqueue((nx, ny));
+                }
+            }
+        }
+
+        return image;
+    }
+
+    private static bool BoundsCheck(int x, int y, int m, int n)
+    {
+        return x >= 0 &&
+               x < m &&
+               y >= 0 &&
+               y < n;
+    }
+}
