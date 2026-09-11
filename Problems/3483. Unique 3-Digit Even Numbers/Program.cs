2061173e4d054ef8c19fa9c35a0f6f3251589632
TransportUnit <65557505+TransportUnit@users.noreply.github.com>
2026-09-11 11:57:40 +0200

    added 3483

---
+++ Problems/3483. Unique 3-Digit Even Numbers/Program.cs
@@ -0,0 +1,18 @@
+using LeetCodeDaily.Core;
+using LeetCodeDaily.Extensions;
+
+"""
+[1,2,3,4]
+12
+
+[0,2,2]
+2
+
+[6,6,6]
+1
+
+[1,3,5]
+0
+"""
+.ParseCases<int[], int>()
+.DetectAndRun();
---
+++ Problems/3483. Unique 3-Digit Even Numbers/Solution.cs
@@ -0,0 +1,44 @@
+using LeetCodeDaily.Core;
+
+namespace _3483.Unique_3_Digit_Even_Numbers;
+
+public class Solution
+{
+    [ResultGenerator]
+    public int TotalNumbers(int[] digits)
+    {
+        int count = 0;
+
+        int n = digits.Length;
+
+        Span<bool> vis = stackalloc bool[1000];
+
+        for (int i = 0; i < n; i++)
+        {
+            if (digits[i] == 0)
+                continue;
+
+            for (int j = 0; j < n; j++)
+            {
+                if (j == i)
+                    continue;
+
+                for (int k = 0; k < n; k++)
+                {
+                    if (k == i || k == j || digits[k] % 2 != 0)
+                        continue;
+
+                    int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+
+                    if (!vis[num])
+                    {
+                        vis[num] = true;
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
